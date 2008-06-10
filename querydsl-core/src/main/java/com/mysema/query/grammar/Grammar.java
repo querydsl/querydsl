777084a1342a3daec9b9512608c6eeeef943b5e0
Timo Westkämper <timo.westkamper@mysema.com>
2008-06-10 17:57:08 +0000

    made type signature for arithmetic operations use Number & Comparable

--- querydsl-core/src/main/java/com/mysema/query/grammar/Grammar.java
+++ querydsl-core/src/main/java/com/mysema/query/grammar/Grammar.java
@@ -21,19 +21,19 @@ import com.mysema.query.grammar.types.Expr;
  */
 public class Grammar {
     
-    public static <A extends Comparable<A>> Expr.Comparable<A> add(Expr<A> left, A right) {
+    public static <A extends Number & Comparable<A>> Expr.Comparable<A> add(Expr<A> left, A right) {
         return createNumber(Ops.ADD, left, createConstant(right));
     }
     
-    public static <A extends Comparable<A>> Expr.Comparable<A> add(Expr<A> left, Expr<A> right) {
+    public static <A extends Number & Comparable<A>> Expr.Comparable<A> add(Expr<A> left, Expr<A> right) {
         return createNumber(Ops.ADD, left, right);
     }
 
-    public static <A extends Comparable<A>> Expr.Comparable<A> div(Expr<A> left, A right) {
+    public static <A extends Number & Comparable<A>> Expr.Comparable<A> div(Expr<A> left, A right) {
         return createNumber(Ops.DIV, left, createConstant(right));
     }
     
-    public static <A extends Comparable<A>> Expr.Comparable<A> div(Expr<A> left, Expr<A> right) {
+    public static <A extends Number & Comparable<A>> Expr.Comparable<A> div(Expr<A> left, Expr<A> right) {
         return createNumber(Ops.DIV, left, right);
     }
     
@@ -41,11 +41,11 @@ public class Grammar {
         return createBoolean(Ops.IN, createConstant(left), (Expr<?>)right);
     }
 
-    public static <A extends Comparable<A>> Expr.Comparable<A> mult(Expr<A> left, A right) {
+    public static <A extends Number & Comparable<A>> Expr.Comparable<A> mult(Expr<A> left, A right) {
         return createNumber(Ops.MULT, left, createConstant(right));
     }
     
-    public static <A extends Comparable<A>> Expr.Comparable<A> mult(Expr<A> left, Expr<A> right) {
+    public static <A extends Number & Comparable<A>> Expr.Comparable<A> mult(Expr<A> left, Expr<A> right) {
         return createNumber(Ops.MULT, left, right);
     }
 
@@ -53,15 +53,15 @@ public class Grammar {
         return createBoolean(Ops.NOT, left);
     }
 
-    public static <A extends Comparable<A>> Expr.Comparable<A> sqrt(Expr<A> left) {
+    public static <A extends Number & Comparable<A>> Expr.Comparable<A> sqrt(Expr<A> left) {
         return createNumber(Ops.SQRT, left);
     }
     
-    public static <A extends Comparable<A>> Expr.Comparable<A> sub(Expr<A> left, A right) {
+    public static <A extends Number & Comparable<A>> Expr.Comparable<A> sub(Expr<A> left, A right) {
         return createNumber(Ops.SUB, left, createConstant(right));
     }
 
-    public static <A extends Comparable<A>> Expr.Comparable<A> sub(Expr<A> left, Expr<A> right) {
+    public static <A extends Number & Comparable<A>> Expr.Comparable<A> sub(Expr<A> left, Expr<A> right) {
         return createNumber(Ops.SUB, left, right);
     }
     
