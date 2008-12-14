65dff369ac95bdc082d4b5bab6aa12077ae35ae9
Timo Westkämper <timo.westkamper@mysema.com>
2008-12-14 09:31:49 +0000

    made constants of MiniApi final

--- querydsl-collections/src/main/java/com/mysema/query/collections/MiniApi.java
+++ querydsl-collections/src/main/java/com/mysema/query/collections/MiniApi.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2008 Mysema Ltd.
+ * All rights reserved.
+ * 
+ */
 package com.mysema.query.collections;
 
 import com.mysema.query.grammar.Grammar;
@@ -15,17 +20,17 @@ import com.mysema.query.grammar.types.PathMetadata;
  */
 public class MiniApi {
     
-    private static PathMetadata<String> pm = PathMetadata.forVariable("it");
+    private static final PathMetadata<String> pm = PathMetadata.forVariable("it");
     
-    public static Path<Object> it = new Path.Entity<Object>(Object.class,"it");
+    public static final Path<Object> it = new Path.Entity<Object>(Object.class,"it");
     
-    public static Path.Comparable<Byte> byteVal = new Path.Comparable<Byte>(Byte.class, pm);
-    public static Path.Comparable<Integer> intVal = new Path.Comparable<Integer>(Integer.class, pm);   
-    public static Path.Comparable<Long> longVal = new Path.Comparable<Long>(Long.class, pm);
-    public static Path.Comparable<Float> floatVal = new Path.Comparable<Float>(Float.class, pm);
-    public static Path.Comparable<Double> doubleVal = new Path.Comparable<Double>(Double.class, pm);
+    public static final Path.Comparable<Byte> byteVal = new Path.Comparable<Byte>(Byte.class, pm);
+    public static final Path.Comparable<Integer> intVal = new Path.Comparable<Integer>(Integer.class, pm);   
+    public static final Path.Comparable<Long> longVal = new Path.Comparable<Long>(Long.class, pm);
+    public static final Path.Comparable<Float> floatVal = new Path.Comparable<Float>(Float.class, pm);
+    public static final Path.Comparable<Double> doubleVal = new Path.Comparable<Double>(Double.class, pm);
     
-    public static Path.String stringVal = new Path.String(pm), str = stringVal;
+    public static final Path.String stringVal = new Path.String(pm), str = stringVal;
     
     @SuppressWarnings("unchecked")
     public static <A> Iterable<A> select(Iterable<A> from, Expr.Boolean where, OrderSpecifier<?>... order){
