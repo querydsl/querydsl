108d5fcc5b0ca2975519d6f2997cefcdc4bbb619
Timo Westkämper <timo.westkamper@mysema.com>
2008-12-17 08:43:47 +0000

    added javadocs for packages

---
+++ querydsl-collections/src/main/java/com/mysema/query/collections/iterators/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2008 Mysema Ltd.
+ * All rights reserved.
+ * 
+ */
+
+/**
+ * Grammar extensions and serialization for Querydsl collections
+ */
+package com.mysema.query.collections.iterators;
+
---
+++ querydsl-collections/src/main/java/com/mysema/query/collections/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2008 Mysema Ltd.
+ * All rights reserved.
+ * 
+ */
+
+/**
+ * Querydsl for Java collections
+ */
+package com.mysema.query.collections;
+
