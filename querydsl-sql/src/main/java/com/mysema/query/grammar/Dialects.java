30113d8ac9bc4a72a2ca0b882fe35bc5a56bcb7c
Timo Westkämper <timo.westkamper@mysema.com>
2009-01-14 15:36:20 +0000

    added javadoc headers

--- querydsl-sql/src/main/java/com/mysema/query/grammar/Dialects.java
+++ querydsl-sql/src/main/java/com/mysema/query/grammar/Dialects.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2008 Mysema Ltd.
+ * All rights reserved.
+ * 
+ */
 package com.mysema.query.grammar;
 
 /**
@@ -9,5 +14,30 @@ package com.mysema.query.grammar;
 public class Dialects {
     
     public static final SqlOps HSQLDB = new SqlOps();
+    
+//    "HSQL Database Engine", HSQLDialect.class
+//    "H2", H2Dialect.class
+//    "MySQL", MySQLDialect.class
+//    "PostgreSQL", PostgreSQLDialect.class
+//    "Apache Derby", DerbyDialect.class
+//    "Ingres", IngresDialect.class
+//    "ingres", IngresDialect.class
+//    "INGRES", IngresDialect.class
+//    "Microsoft SQL Server Database", SQLServerDialect.class
+//    "Microsoft SQL Server", SQLServerDialect.class
+//    "Sybase SQL Server", SybaseDialect.class
+//    "Adaptive Server Enterprise", SybaseDialect.class
+//    "Informix Dynamic Server", InformixDialect.class
+//    "DB2/NT", DB2Dialect.class
+//    "DB2/LINUX", DB2Dialect.class
+//    "DB2/6000", DB2Dialect.class
+//    "DB2/HPUX", DB2Dialect.class
+//    "DB2/SUN", DB2Dialect.class
+//    "DB2/LINUX390", DB2Dialect.class
+//    "DB2/AIX64", DB2Dialect.class
+//    "Oracle", 8, Oracle8iDialect.class
+//    "Oracle", 9, Oracle9iDialect.class
+//    "Oracle", 10, Oracle10gDialect.class
+//    "Oracle", 11, Oracle10gDialect.class
     
 }
--- querydsl-sql/src/test/java/com/mysema/query/sql/dto/QIdName.java
+++ querydsl-sql/src/test/java/com/mysema/query/sql/dto/QIdName.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2008 Mysema Ltd.
+ * All rights reserved.
+ * 
+ */
 package com.mysema.query.sql.dto;
 
 import com.mysema.query.grammar.types.Constructor;
