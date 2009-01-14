4dc3d005fa6170bd3f6c77f21a3aed1aa906be4c
Timo Westkämper <timo.westkamper@mysema.com>
2009-01-14 12:53:12 +0000

    moved commons-lang dependency to querydsl-core added support for java constructor retrieval from Constructor expression

--- querydsl-core/src/main/java/com/mysema/query/grammar/types/Constructor.java
+++ querydsl-core/src/main/java/com/mysema/query/grammar/types/Constructor.java
@@ -5,8 +5,11 @@
  */
 package com.mysema.query.grammar.types;
 
+import org.apache.commons.lang.ClassUtils;
+
 public class Constructor<D> extends Expr<D> {
     private final Expr<?>[] args;
+    private java.lang.reflect.Constructor<D> javaConstructor;
     public Constructor(Class<D> type, Expr<?>... args) {
         super(type);
         this.args = args;
@@ -25,4 +28,33 @@ public class Constructor<D> extends Expr<D> {
             return elementType;
         }
     }
+    
+    @SuppressWarnings("unchecked")
+    public java.lang.reflect.Constructor<D> getJavaConstructor(){   
+        if (javaConstructor == null){
+            Class<D> type = getType();
+            Expr<?>[] args = getArgs();
+            for (java.lang.reflect.Constructor<?> c : type.getConstructors()){
+                if (c.getParameterTypes().length == args.length){
+                    boolean match = true;
+                    for (int i = 0; i < args.length && match; i++){
+                        Class<?> ptype = c.getParameterTypes()[i];
+                        if (ptype.isPrimitive()){
+                            ptype = ClassUtils.primitiveToWrapper(ptype);
+                        }
+                        match &= ptype.isAssignableFrom(args[i].getType());                                        
+                    }
+                    if (match){
+                        javaConstructor = (java.lang.reflect.Constructor<D>)c;
+                        return javaConstructor;
+                    }
+                }
+            }
+            throw new IllegalArgumentException("no suitable constructor found");       
+        }else{
+            return javaConstructor;
+        }
+         
+    }
+
 }
