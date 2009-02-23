51f6a2e48632627c744ebdcae13fcf02ce521e02
Timo Westkämper <timo.westkamper@mysema.com>
2009-02-23 14:35:02 +0000

    fixed oracle dialect issues

--- querydsl-sql/src/main/java/com/mysema/query/grammar/Dialect.java
+++ querydsl-sql/src/main/java/com/mysema/query/grammar/Dialect.java
@@ -7,6 +7,7 @@ package com.mysema.query.grammar;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.sql.Types;
 
 
 /**
@@ -49,33 +50,7 @@ public class Dialect {
     
     // tested
     public static SqlOps forOracle(){
-        return new SqlOps(){{
-            add(Ops.OpMath.CEIL, "ceil(%s)");  
-            add(Ops.OpMath.RANDOM, "dbms_random.value");
-            add(Ops.OpMath.LOG, "ln(%s)");  
-            add(Ops.OpMath.LOG10, "log(10,%s)");
-            
-            add(Ops.CONCAT, "%s || %s");
-            add(Ops.OpString.SPACE, "lpad('',%s,' ')");
-                      
-            add(Ops.OpDateTime.YEAR, "extract(year from %s)");
-            add(Ops.OpDateTime.MONTH, "extract(month from %s)");
-            add(Ops.OpDateTime.WEEK, "to_number(to_char(%s,'WW'))");
-            add(Ops.OpDateTime.DAY, "extract(day from %s)");
-            
-            add(Ops.OpDateTime.HOUR, "to_number(to_char(%s,'HH24'))");
-            add(Ops.OpDateTime.MINUTE, "to_number(to_char(%s,'MI'))");
-            add(Ops.OpDateTime.SECOND, "to_number(to_char(%s,'SS'))");
-            
-            add(Ops.OpDateTime.DAY_OF_MONTH, "to_number(to_char(%s,'DD'))");
-            add(Ops.OpDateTime.DAY_OF_WEEK, "to_number(to_char(%s,'D'))");
-            add(Ops.OpDateTime.DAY_OF_YEAR, "to_number(to_char(%s,'DDD'))");
-            
-            limitAndOffsetSymbols(false);
-            limitTemplate("rownum < %s");
-            offsetTemplate("rownum > %s");
-            limitOffsetTemplate("rownum between %1$s and %3$s");
-        }};
+        return new OracleDialect();
     }
     
     // TODO : to be tested
