99dff3abe9da3614db793a7298e6a6ebb53288f6
Timo Westkämper <timo.westkamper@mysema.com>
2009-03-06 13:15:44 +0000

    fixed some generics issues in APT based query type generation

---
+++ querydsl-collections/src/test/java/com/mysema/query/DomainTest.java
@@ -0,0 +1,80 @@
+package com.mysema.query;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+import org.junit.Test;
+
+import com.mysema.query.annotations.Domain;
+
+/**
+ * Domain provides
+ *
+ * @author tiwe
+ * @version $Id$
+ */
+public class DomainTest {
+    
+    @Test
+    @SuppressWarnings("unused")
+    public void test(){        
+        QSimpleTypes st = new QSimpleTypes("st");        
+        QRelationType rt = new QRelationType("rt");
+        
+        QRelationType2 rt2 = new QRelationType2("rt2");        
+        rt2 = rt2.list(0);
+        
+        QGenericType gt = new QGenericType("qt");
+        QItemType it = gt.itemType;
+        it = new QItemType("it");
+    }
+    
+    @Domain
+    public static class SimpleTypes{
+        transient int test;
+        long id;
+        BigDecimal bigDecimal;
+        Byte bbyte;
+        byte bbyte2;
+        Character cchar;
+        char cchar2;
+        Double ddouble;
+        double ddouble2;
+        Float ffloat;
+        float ffloat2;
+        Integer iint;
+        int iint2;
+        Locale llocale;
+        Long llong;
+        long llong2;
+        String sstring;
+        Date date;
+        java.sql.Time time;
+        java.sql.Timestamp timestamp;
+    }
+    
+    @Domain
+    public static class RelationType{
+        List<RelationType> list;
+        List<? extends RelationType> list2;
+        Set<RelationType> set;        
+        SortedSet<RelationType> sortedSet;
+        Collection<RelationType> collection;
+    }
+    
+    @Domain
+    public static class RelationType2<D extends RelationType2>{
+        List<D> list;
+    }
+    
+    @Domain
+    public class GenericType<T extends ItemType>{
+        T itemType;        
+    }        
+    
+    @Domain
+    public class ItemType{
+        
+    }
+    
+}
