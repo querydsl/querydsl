a2d1859ebd9c7b10c4cffe901dbd227c5e7925d8
Timo Westkämper <timo.westkamper@mysema.com>
2009-03-11 13:54:49 +0000

    separate apt types into apt specific and model

--- querydsl-sql/src/main/java/com/mysema/query/sql/MetaDataExporter.java
+++ querydsl-sql/src/main/java/com/mysema/query/sql/MetaDataExporter.java
@@ -91,7 +91,8 @@ public class MetaDataExporter {
 //            if (camelCase){
 //                tableName = toCamelCase(tableName, true);
 //            }
-            Type type = new Type(null, "java.lang.Object",tableName);
+//            String superType, String packageName, String name, String simpleName
+            Type type = new Type(null, "java.lang", "java.lang.Object", tableName);
             ResultSet columns = md.getColumns(null, schemaPattern, tables.getString(3), null);
             while (columns.next()){
                 String _name = columns.getString(4);
@@ -108,9 +109,11 @@ public class MetaDataExporter {
                 }else{
                     _type = Field.Type.COMPARABLE;
                 }
+//                String name, String realName, String keyTypeName, String typePackage, String typeName
                 type.addField(new Field(
                         _name, columns.getString(4), null, 
-                        _class.getName(), _class.getSimpleName(), _type));
+                        _class.getPackage().getName(), _class.getName(), _class.getSimpleName(),
+                        _type));
             }
             columns.close();
             serialize(type);
