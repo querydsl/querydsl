24f4b65e52d426262a09199cf9b052b7c9954d28
Timo Westkämper <timo.westkamper@mysema.com>
2009-10-01 12:26:23 +0000

    added javadocs

--- querydsl-core/src/main/java/com/mysema/query/annotations/PropertyType.java
+++ querydsl-core/src/main/java/com/mysema/query/annotations/PropertyType.java
@@ -1,32 +1,34 @@
 package com.mysema.query.annotations;
 
 /**
+ * PropertyType defines the Path type to be used for a Domain property
+ * 
  * @author tiwe
  *
  */
 public enum PropertyType {
     /**
-     * 
+     *  for PDate fields
      */
     DATE,
     /**
-     * 
+     *  for PDateTime fields
      */
     DATETIME,
     /**
-     * 
+     *  to skip properties
      */
     NONE,
     /**
-     * 
+     * for PNumber fields
      */
     NUMERIC,
     /**
-     * 
+     * for PSimple fields
      */
     SIMPLE,
     /**
-     * 
+     * for PTime fields
      */
     TIME
     
--- querydsl-core/src/main/java/com/mysema/query/annotations/QueryType.java
+++ querydsl-core/src/main/java/com/mysema/query/annotations/QueryType.java
@@ -8,10 +8,19 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * QueryType defines the Querydsl property type for a Domain property
+ * 
+ * @author tiwe
+ *
+ */
 @Documented
 @Target({FIELD,METHOD})
 @Retention(RUNTIME)
 public @interface QueryType {
 
+    /**
+     * @return
+     */
     PropertyType value();
 }
