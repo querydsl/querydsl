ae50651319337afd3054fa874b880f1fbce6662d
Timo Westkämper <timo.westkamper@mysema.com>
2009-10-26 14:04:29 +0000

    added test for cache

---
+++ querydsl-apt/src/test/java/com/mysema/query/domain/PathMetadataTest.java
@@ -0,0 +1,47 @@
+package com.mysema.query.domain;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mysema.query.types.expr.EString;
+
+public class PathMetadataTest {
+    
+    @Before
+    public void setUp(){
+        assertNotNull(QAnimal.animal);
+        assertNotNull(QCat.cat);
+        assertNotNull(QCategory.category);
+        assertNotNull(QSimpleTypes.simpleTypes);
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Test
+    public void test() throws Exception{        
+        Field field = EString.class.getDeclaredField("cache");
+        field.setAccessible(true);
+        Map<String, EString> cache = (Map) field.get(null);
+        System.out.println(cache.size() + " entries in EString cache");
+        
+        // numbers
+        assertTrue(cache.containsKey("0"));
+        assertTrue(cache.containsKey("10"));
+        
+        // variables
+        assertTrue(cache.containsKey("animal"));
+        assertTrue(cache.containsKey("cat"));
+        assertTrue(cache.containsKey("category"));
+        assertTrue(cache.containsKey("simpleTypes"));
+        
+        // properties
+        assertTrue(cache.containsKey("mate"));
+        
+    }
+
+}
