e3ec20b2bb3f63053a071393538d805aaec5a266
Timo Westkämper <timo.westkamper@mysema.com>
2010-03-16 07:44:41 +0000

    fixed issue with TypeUtil

--- querydsl-core/src/main/java/com/mysema/query/codegen/TypeUtil.java
+++ querydsl-core/src/main/java/com/mysema/query/codegen/TypeUtil.java
@@ -2,20 +2,20 @@ package com.mysema.query.codegen;
 
 public final class TypeUtil {
 
-    public static Type transform(Type type, Type declaringType, EntityType model){
+    public static Type transform(Type type, Type declaringType, EntityType context){
         if (type instanceof TypeExtends){
             TypeExtends extendsType = (TypeExtends)type;
             if (extendsType.getVarName() != null){
-                type = extendsType.resolve(model, declaringType);
+                type = extendsType.resolve(context, declaringType);
             }            
         }
 
-        if(type.getParameterCount() > 0){
+        if(type != null && type.getParameterCount() > 0){
             Type[] params = new Type[type.getParameterCount()];
             boolean transformed = false;
             for (int i = 0; i < type.getParameterCount(); i++){
                 Type param = type.getParameter(i);
-                params[i] = transform(param, declaringType, model);
+                params[i] = transform(param, declaringType, context);
                 if (params[i] != param){
                     transformed = true;
                 }
