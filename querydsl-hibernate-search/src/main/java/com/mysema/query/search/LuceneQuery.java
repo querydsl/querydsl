376e4a1ceb9111140084841171ae1bd4e4dc746a
Timo Westkämper <timo.westkamper@mysema.com>
2010-03-16 08:57:49 +0000

    added stub for querydsl-hibernate-search

---
+++ querydsl-hibernate-search/src/main/java/com/mysema/query/search/LuceneQuery.java
@@ -0,0 +1,96 @@
+package com.mysema.query.search;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import com.mysema.query.QueryModifiers;
+import com.mysema.query.SearchResults;
+import com.mysema.query.SimpleProjectable;
+import com.mysema.query.SimpleQuery;
+import com.mysema.query.support.QueryMixin;
+import com.mysema.query.types.expr.EBoolean;
+import com.mysema.query.types.path.Path;
+
+/**
+ * @author tiwe
+ *
+ * @param <T>
+ */
+public class LuceneQuery<T> implements SimpleQuery<LuceneQuery<T>>, SimpleProjectable<T>{
+
+    private final Session session;
+    
+    private final Path<?> entityPath;
+    
+    private final QueryMixin<LuceneQuery<T>> queryMixin;
+    
+    public LuceneQuery(Session session, Path<?> entityPath) {
+        this.session = session;
+        this.entityPath = entityPath;
+        this.queryMixin = new QueryMixin<LuceneQuery<T>>(this);
+    }
+
+    @Override
+    public LuceneQuery<T> limit(long limit) {
+        return queryMixin.limit(limit);
+    }
+
+    @Override
+    public LuceneQuery<T> offset(long offset) {
+        return queryMixin.offset(offset);
+    }
+
+    @Override
+    public LuceneQuery<T> restrict(QueryModifiers modifiers) {
+        return queryMixin.restrict(modifiers);
+    }
+
+    @Override
+    public LuceneQuery<T> where(EBoolean... e) {
+        return queryMixin.where(e);
+    }
+
+    @Override
+    public long count() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public long countDistinct() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public List<T> list() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<T> listDistinct() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public SearchResults<T> listDistinctResults() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public SearchResults<T> listResults() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T uniqueResult() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ querydsl-hibernate-search/src/test/java/com/mysema/query/search/SimpleTest.java
@@ -0,0 +1,97 @@
+package com.mysema.query.search;
+
+import org.hibernate.Session;
+import org.junit.Test;
+
+import com.mysema.query.types.path.PString;
+import com.mysema.query.types.path.PathBuilder;
+
+
+public class SimpleTest {
+
+    @Test
+    public void specs(){
+        Session session = null;
+        PathBuilder<Object> entityPath = new PathBuilder<Object>(Object.class, "obj");
+        PString stringPath = entityPath.getString("prop");
+        
+        LuceneQuery<Object> query = new LuceneQuery<Object>(session, entityPath);
+        
+//        Terms & Phrases
+//        "test" or "hello dolly"
+//        c.Match("test") or c.Match("hello dolly")
+        stringPath.like("test").or(stringPath.like("hello dolly"));
+        
+//        Fields
+//        title:"The Right way" and text:go
+//        c.Title == "The Right way" and c.Text == "go"
+        stringPath.eq("The Right Way").and(stringPath.eq("go"));
+        
+//        WildCard
+//        amb?r
+//        c.ContactName.Match("amb?r")
+        stringPath.like("amb?r");
+        
+//        Prefix
+//        amber*
+//        c.ContactName.StartsWith("amber")
+        stringPath.startsWith("amber");
+        
+//        Fuzzy
+//        roam~ or roam~0.8
+//        c.ContactName.Like("roam") or c.ContactName.Like("roam", 0.8)
+        // TODO
+        
+//        Proximity
+//        "jakarta apache"~10
+//        c.ContactName.Like("jakarta apache", 10)
+        // TODO
+        
+//        Inclusive Range
+//        mod_date:[20020101 TO 20030101]
+//        c.ModifiedDate.Includes("20020101", "20030101")
+//        stringPath.between("20020101", "20030101");
+        // TODO
+        
+//        Exclusive Range
+//        title:{Aida TO Carmen}
+//        c.Title.Between("Aida", "Carmen")
+        stringPath.between("Aida", "Carmen");
+        
+//        Boosting
+//        jakarta^4 apache
+//        c.Title.Match("jakarta".Boost(4), apache)
+        // TODO
+        
+//        Boolean Or
+//        "jakarta apache" OR jakarta
+//        where c.Match("jakarta apache") || c.Match("jakarta")
+        stringPath.like("jakarta apache").or(stringPath.like("jakarta"));
+        
+//        Boolean And
+//        "jakarta apache" AND "Apache Lucene"
+//        where c.Match("jakarta apache") && c.Match("Apache Lucene")
+        stringPath.like("jakarta apache").and(stringPath.like("Apache Lucene"));
+        
+//        Boolean Not
+//        "jakarta apache" NOT "Apache Lucene"
+//        where c.Match("jakarta apache") && !c.Match("Apache Lucene")
+        stringPath.like("jakarta apache").and(stringPath.like("Apache Lucene").not());
+        
+//        Required
+//        +jakarta lucene
+//        c.Title.Match("jakarta".Require(), "lucene")
+//         TODO
+        
+//        Grouping
+//        (jakarta OR apache) AND website
+//        where (c.Title == "jakarta" || c.Title == "apache") && (c.Title == "website")
+        stringPath.eq("jakarta").or(stringPath.eq("apache")).and(stringPath.eq("website"));
+        
+//        Native syntax
+//        ie. title:{+return +"pink panther")
+//        c.Search("title:(return +\"pink panther\"")
+//        TODO
+    }
+    
+}
