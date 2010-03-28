6341c485609e0c33e89e679be8efc525230dcfc8
Timo Westkämper <timo.westkamper@mysema.com>
2010-03-28 21:16:48 +0000

    added @SuppressWarnings("deprecation")

--- querydsl-core/src/main/java/com/mysema/util/ResultSetAdapter.java
+++ querydsl-core/src/main/java/com/mysema/util/ResultSetAdapter.java
@@ -91,6 +91,7 @@ public class ResultSetAdapter implements ResultSet{
         return rs.getAsciiStream(columnLabel);
     }
 
+    @SuppressWarnings("deprecation")
     public BigDecimal getBigDecimal(int columnIndex, int scale) throws SQLException {
         return rs.getBigDecimal(columnIndex, scale);
     }
@@ -99,6 +100,7 @@ public class ResultSetAdapter implements ResultSet{
         return rs.getBigDecimal(columnIndex);
     }
 
+    @SuppressWarnings("deprecation")
     public BigDecimal getBigDecimal(String columnLabel, int scale) throws SQLException {
         return rs.getBigDecimal(columnLabel, scale);
     }
@@ -363,10 +365,12 @@ public class ResultSetAdapter implements ResultSet{
         return rs.getType();
     }
 
+    @SuppressWarnings("deprecation")
     public InputStream getUnicodeStream(int columnIndex) throws SQLException {
         return rs.getUnicodeStream(columnIndex);
     }
 
+    @SuppressWarnings("deprecation")
     public InputStream getUnicodeStream(String columnLabel) throws SQLException {
         return rs.getUnicodeStream(columnLabel);
     }
