0fd49f058c3cd5a1af1c934e8a5f23442e0c0bd4
Vesa Martilla <vesa.marttila@mysema.com>
2010-03-29 07:21:32 +0000

    Added @Override for SearchQueryTest.setUp.

--- querydsl-hibernate-search/src/test/java/com/mysema/query/search/SearchQueryTest.java
+++ querydsl-hibernate-search/src/test/java/com/mysema/query/search/SearchQueryTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2010 Mysema Ltd.
  * All rights reserved.
- * 
+ *
  */
 package com.mysema.query.search;
 
@@ -21,72 +21,73 @@ import com.mysema.query.types.expr.EBoolean;
 
 
 public class SearchQueryTest extends AbstractQueryTest{
-    
+
     private QUser user = new QUser("user");
-    
+
+    @Override
     public void setUp(){
-        super.setUp();        
+        super.setUp();
         createUser("Bob", "Stewart","Smith", "bob@example.com");
-        
+
         createUser("Barbara","X","Lock","barbara@a.com");
-        createUser("Anton","X","Bruckner","anton@b.com");                
+        createUser("Anton","X","Bruckner","anton@b.com");
         createUser("Robert","X","Downing","bob@c.com");
         createUser("John","X","Stewart","john@d.com");
-        
+
         Session session = getSession();
         session.flush();
-        session.getTransaction().commit();        
+        session.getTransaction().commit();
         session.beginTransaction();
     }
-    
+
     @Test
     public void basicTests(){
         EBoolean filter = user.emailAddress.eq("bob@example.com");
-        
+
         // count
         assertEquals(1, query().where(filter).count());
-        
+
         // uniqueResult
         User u = query().where(filter).uniqueResult();
         assertNotNull(u);
         assertEquals("bob@example.com", u.getEmailAddress());
-        
+
         // list
         List<User> list = query().where(filter).list();
         assertEquals(1, list.size());
         assertEquals(u, list.get(0));
     }
-     
+
     @Test
     public void ordering(){
         EBoolean filter = user.middleName.eq("X");
         // asc
         List<String> asc = getFirstNames(query().where(filter).orderBy(user.firstName.asc()).list());
         assertEquals(Arrays.asList("Anton","Barbara","John","Robert"), asc);
-        
+
         // desc
         List<String> desc = getFirstNames(query().where(filter).orderBy(user.firstName.desc()).list());
-        assertEquals(Arrays.asList("Robert","John","Barbara","Anton"), desc);        
+        assertEquals(Arrays.asList("Robert","John","Barbara","Anton"), desc);
     }
-    
+
     @Test
     public void paging(){
         EBoolean filter = user.middleName.eq("X");
         OrderSpecifier<?> order = user.firstName.asc();
-        
+
         // limit
         List<String> limit = getFirstNames(query().where(filter).orderBy(order).limit(2).list());
         assertEquals(Arrays.asList("Anton","Barbara"), limit);
-        
+
         // offset
         List<String> offset = getFirstNames(query().where(filter).orderBy(order).offset(1).list());
         assertEquals(Arrays.asList("Barbara","John","Robert"), offset);
-        
+
         // limit + offset
         List<String> limitAndOffset = getFirstNames(query().where(filter).orderBy(order).limit(2).offset(1).list());
         assertEquals(Arrays.asList("Barbara","John"), limitAndOffset);
     }
-    
+
     @Test
     public void listResults(){
         EBoolean filter = user.middleName.eq("X");
@@ -113,8 +114,8 @@ public class SearchQueryTest extends AbstractQueryTest{
         getSession().save(user);
         return user;
     }
-    
-    private SearchQuery<User> query(){        
+
+    private SearchQuery<User> query(){
         return new SearchQuery<User>(getSession(), user);
     }
 
