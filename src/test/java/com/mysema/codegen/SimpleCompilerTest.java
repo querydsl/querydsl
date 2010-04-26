72ebde8fb387377e167bde8fa5e81f51198047ee
Timo Westkämper <timo.westkamper@mysema.com>
2010-04-26 11:08:30 +0000

    added test for SimpleCompiler

---
+++ src/test/java/com/mysema/codegen/SimpleCompilerTest.java
@@ -0,0 +1,30 @@
+package com.mysema.codegen;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.tools.JavaCompiler;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class SimpleCompilerTest {
+
+    @Test
+    public void testRun() {
+        new File("target/out").mkdir();
+        JavaCompiler compiler = new SimpleCompiler();
+        System.out.println(compiler.getClass().getName());
+        List<String> options = new ArrayList<String>(3);
+        options.add("-s");
+        options.add("target/out");
+        options.add("src/test/java/com/mysema/codegen/SimpleCompilerTest.java");
+        int compilationResult = compiler.run(null, null, null, options.toArray(new String[options.size()]));
+        if(compilationResult != 0){
+            Assert.fail("Compilation Failed");
+        }
+    }
+
+}
