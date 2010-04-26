6e93720a48d380f57196137cf897a0a6d4464e1d
Timo Westkämper <timo.westkamper@mysema.com>
2010-04-26 11:32:23 +0000

    added tests

---
+++ src/test/java/com/mysema/codegen/MemSourceFileObjectTest.java
@@ -0,0 +1,24 @@
+package com.mysema.codegen;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class MemSourceFileObjectTest {
+    
+    @Test
+    public void test(){
+        MemSourceFileObject obj = new MemSourceFileObject("Test", "Hello World");
+        assertEquals("Hello World", obj.getCharContent(true).toString());
+    }
+
+    @Test
+    public void testOpenWriter() throws IOException {
+        MemSourceFileObject obj = new MemSourceFileObject("Test");
+        obj.openWriter().write("Hello World");
+        assertEquals("Hello World", obj.getCharContent(true).toString());
+    }
+
+}
