5be983747a0a07aa60f072ed2e89f48d45300ecb
Timo Westkämper <timo.westkamper@mysema.com>
2010-05-12 19:26:04 +0000

    improved tests

--- querydsl-lucene/src/test/java/com/mysema/query/lucene/QueryElementTest.java
+++ querydsl-lucene/src/test/java/com/mysema/query/lucene/QueryElementTest.java
@@ -11,8 +11,11 @@ public class QueryElementTest {
 
     @Test
     public void test(){
-        QueryElement element = new QueryElement(new TermQuery(new Term("str","text")));
+        QueryElement element = new QueryElement(new TermQuery(new Term("str","text")));        
         assertEquals("str:text",element.toString());
         assertEquals(element.getQuery().hashCode(), element.hashCode());
+        
+        QueryElement element2 = new QueryElement(new TermQuery(new Term("str","text")));
+        assertEquals(element2, element);
     }
 }
