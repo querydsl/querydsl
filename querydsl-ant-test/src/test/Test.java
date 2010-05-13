4abca1a0ecba284deb344982d932c81b6e600c42
Timo Westkämper <timo.westkamper@mysema.com>
2010-05-13 20:58:48 +0000

    added contents

---
+++ querydsl-ant-test/src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+import java.math.BigDecimal;
+
+import com.mysema.query.annotations.QueryEntity;
+
+
+@QueryEntity
+public class Test {
+    
+    private String property;
+
+    private int intProperty;
+    
+    private BigDecimal bigDecimal;
+}
