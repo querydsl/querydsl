18235ec5de6baafe9f6ae1147c916071eb636de2
Timo Westkämper <timo.westkamper@mysema.com>
2010-05-15 19:28:53 +0000

    added tests

--- querydsl-core/src/test/java/com/mysema/query/types/custom/CustomTest.java
+++ querydsl-core/src/test/java/com/mysema/query/types/custom/CustomTest.java
@@ -5,13 +5,17 @@
  */
 package com.mysema.query.types.custom;
 
+import static org.junit.Assert.*;
+
 import java.sql.Time;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
 
+import com.mysema.query.types.Custom;
 import com.mysema.query.types.Expr;
 import com.mysema.query.types.Template;
 import com.mysema.query.types.TemplateFactory;
@@ -23,21 +27,43 @@ public class CustomTest {
     @Test
     public void constructors(){
         Template template = TemplateFactory.DEFAULT.create("");
-        List<Expr<?>> args = Collections.<Expr<?>>emptyList();
-        
-        new CBoolean(template, args);
-        new CComparable<String>(String.class, template, args);
-        new CDate<Date>(Date.class, template, args);
-        new CDateTime<Date>(Date.class, template, args);
-        new CNumber<Integer>(Integer.class, template, args);
-        new CSimple<Object>(Object.class, template, args);
-        new CString(template, args);
-        new CTime<Time>(Time.class, template, args);
+        List<Expr<?>> args = Collections.<Expr<?>>emptyList();        
+        List<Custom<?>> customs = Arrays.<Custom<?>>asList(
+            new CBoolean(template, args),
+            new CComparable<String>(String.class, template, args),
+            new CDate<Date>(Date.class, template, args),
+            new CDateTime<Date>(Date.class, template, args),
+            new CNumber<Integer>(Integer.class, template, args),
+            new CSimple<Object>(Object.class, template, args),
+            new CString(template, args),
+            new CTime<Time>(Time.class, template, args)
+        );
+        for (Custom<?> custom : customs){
+            assertNotNull(custom.asExpr());
+            assertNotNull(custom.getTemplate());
+            assertNotNull(custom.getType());
+            assertNotNull(custom.getArgs());
+        }
     }
 
     @Test
     public void factoryMethods(){
         String template = "";
+        EBoolean arg = EBooleanConst.TRUE;
+        
+        CBoolean.create(template, arg);
+        CComparable.create(String.class, template, arg);
+        CDate.create(Date.class, template, arg);
+        CDateTime.create(Date.class, template, arg);
+        CNumber.create(Integer.class, template, arg);
+        CSimple.create(Object.class, template, arg);
+        CString.create(template, arg);
+        CTime.create(Time.class, template, arg);
+    }
+    
+    @Test
+    public void factoryMethods2(){
+        Template template = TemplateFactory.DEFAULT.create("");
         EBoolean arg = EBooleanConst.TRUE;
         
         CBoolean.create(template, arg);
