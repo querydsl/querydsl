dacffa1e82bb7cb945fc9ebba2ed8b6fe9044679
Timo Westkämper <timo.westkamper@mysema.com>
2010-05-24 18:37:18 +0000

    added tests

---
+++ querydsl-sql/src/test/java/com/mysema/query/sql/oracle/OracleGrammarTest.java
@@ -0,0 +1,26 @@
+package com.mysema.query.sql.oracle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.mysema.query.types.path.PNumber;
+
+
+public class OracleGrammarTest {
+    
+    @Test
+    public void constants(){
+        assertNotNull(OracleGrammar.level);
+        assertNotNull(OracleGrammar.rownum);
+        assertNotNull(OracleGrammar.sysdate);
+    }
+    
+    @Test
+    public void sumOver(){
+        PNumber<Integer> intPath = new PNumber<Integer>(Integer.class, "intPath");
+        SumOver<Integer> sumOver = OracleGrammar.sumOver(intPath).order(intPath).partition(intPath);
+        assertEquals("sum(intPath) over (partition by intPath order by intPath)", sumOver.toString());
+    }
+
+}
