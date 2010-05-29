e837593eb6086a9a6d48b96fb6f7046f4b126d1e
Timo Westkämper <timo.westkamper@mysema.com>
2010-05-29 09:55:07 +0000

    added tests

--- querydsl-apt/src/test/java/com/mysema/query/domain/KeywordsTest.java
+++ querydsl-apt/src/test/java/com/mysema/query/domain/KeywordsTest.java
@@ -15,6 +15,13 @@ public class KeywordsTest {
     }
     
     @Entity
+    public static class From {
+        
+        String from1;
+    }
+                              
+    
+    @Entity
     public static class NonKeyword {
                 
     }
@@ -29,7 +36,11 @@ public class KeywordsTest {
     @Test
     public void test(){
         assertEquals("order1",QKeywordsTest_Order.order.toString());
+        
+        assertEquals("from1", QKeywordsTest_From.from.from1.toString());
+        
         assertEquals("nonKeyword",QKeywordsTest_NonKeyword.nonKeyword.toString());
+        
         assertEquals("distinct1",QKeywordsTest_Distinct.distinct1.toString());
     }
 
