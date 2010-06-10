1c143f2bd6878d7a461b1ef8e0b7af1be0f30fc6
Timo Westkämper <timo.westkamper@mysema.com>
2010-06-10 18:48:03 +0000

    added ScrollableResultsIterator

--- querydsl-hql/src/test/java/com/mysema/query/AbstractHibernateTest.java
+++ querydsl-hql/src/test/java/com/mysema/query/AbstractHibernateTest.java
@@ -1,23 +1,28 @@
 /*
- * Copyright (c) 2009 Mysema Ltd.
+ * Copyright (c) 2010 Mysema Ltd.
  * All rights reserved.
  * 
  */
 package com.mysema.query;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.hibernate.LockMode;
 import org.hibernate.Session;
 import org.junit.Test;
 
+import com.mysema.commons.lang.CloseableIterator;
 import com.mysema.query.hql.HQLTemplates;
 import com.mysema.query.hql.domain.Cat;
 import com.mysema.query.hql.domain.QCat;
 import com.mysema.query.hql.hibernate.HibernateQuery;
+import com.mysema.query.hql.hibernate.ScrollableResultsIterator;
 
 /**
  * @author tiwe
@@ -56,7 +61,31 @@ public abstract class AbstractHibernateTest extends AbstractStandardTest{
     
     @Test
     public void lockMode(){
-        query().from(QCat.cat).setLockMode(QCat.cat, LockMode.UPGRADE).list(QCat.cat);
+        query().from(QCat.cat).setLockMode(QCat.cat, LockMode.PESSIMISTIC_WRITE).list(QCat.cat);
+    }
+    
+    @Test
+    public void scroll() throws IOException{
+        CloseableIterator<Cat> cats = new ScrollableResultsIterator<Cat>(query().from(QCat.cat).createQuery(QCat.cat).scroll());
+        assertTrue(cats.hasNext());
+        while (cats.hasNext()){
+            assertNotNull(cats.next());
+        }
+        cats.close();               
     }
 
+    @Test
+    public void scrollArray() throws IOException{
+        CloseableIterator<Object[]> rows = new ScrollableResultsIterator<Object[]>(query()
+                .from(QCat.cat)
+                .createQuery(QCat.cat.name, QCat.cat.birthdate).scroll(),true);
+        assertTrue(rows.hasNext());
+        while (rows.hasNext()){
+            Object[] row = rows.next();
+            assertEquals(2, row.length);
+            assertNotNull(row[0]);
+            assertNotNull(row[1]);
+        }
+        rows.close();               
+    }
 }
