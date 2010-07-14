56d05f4c0090121136ab6d0acceebb23b4638422
Timo Westkämper <timo.westkamper@mysema.com>
2010-07-14 14:00:47 +0000

    added tests for MySQL extensions

--- querydsl-sql/src/test/java/com/mysema/query/_mysql/SelectMySQLTest.java
+++ querydsl-sql/src/test/java/com/mysema/query/_mysql/SelectMySQLTest.java
@@ -5,13 +5,17 @@
  */
 package com.mysema.query._mysql;
 
+import static com.mysema.query.Constants.survey;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import com.mysema.query.Connections;
 import com.mysema.query.SelectBaseTest;
 import com.mysema.query.Target;
 import com.mysema.query.sql.MySQLTemplates;
+import com.mysema.query.sql.mysql.MySQLQuery;
 import com.mysema.testutil.Label;
 import com.mysema.testutil.ResourceCheck;
 
@@ -29,4 +33,21 @@ public class SelectMySQLTest extends SelectBaseTest {
         templates = new MySQLTemplates().newLineToSingleSpace();
     }
 
+    @Test
+    public void extensions(){
+        mysqlQuery().from(survey).bigResult().list(survey.id);
+        mysqlQuery().from(survey).bufferResult().list(survey.id);
+        mysqlQuery().from(survey).cache().list(survey.id);        
+        mysqlQuery().from(survey).calcFoundRows().list(survey.id);
+        mysqlQuery().from(survey).noCache().list(survey.id);
+        
+        mysqlQuery().from(survey).highPriority().list(survey.id);
+        mysqlQuery().from(survey).lockInShareMode().list(survey.id);
+        mysqlQuery().from(survey).smallResult().list(survey.id);
+        mysqlQuery().from(survey).straightJoin().list(survey.id);        
+    }
+    
+    private MySQLQuery mysqlQuery(){
+        return new MySQLQuery(Connections.getConnection(), templates);
+    }
 }
