f016bcd22aa8241d0302f4711d062255d7c30b5b
Timo Westkämper <timo.westkamper@mysema.com>
2010-07-16 00:56:57 +0000

    formatted code

--- querydsl-core/src/main/java/com/mysema/query/types/expr/QTuple.java
+++ querydsl-core/src/main/java/com/mysema/query/types/expr/QTuple.java
@@ -11,40 +11,40 @@ import com.mysema.query.types.Expr;
 
 /**
  * QTuple represents a projection of type Tuple
- *
+ * 
  * @author tiwe
- *
+ * 
  */
-public class QTuple extends EConstructor<Tuple>{
+public class QTuple extends EConstructor<Tuple> {
 
     public QTuple(Expr<?>... args) {
-    super(Tuple.class, new Class[0], args);
+        super(Tuple.class, new Class[0], args);
     }
 
     private static final long serialVersionUID = -2640616030595420465L;
 
     @SuppressWarnings("unchecked")
     @Override
-    public Tuple newInstance(final Object... args){
-    return new Tuple(){
+    public Tuple newInstance(final Object... args) {
+        return new Tuple() {
 
             @Override
             public <T> T get(int index, Class<T> type) {
-            return (T) args[index];
+                return (T) args[index];
             }
 
             @Override
             public <T> T get(Expr<T> expr) {
-            int index = getArgs().indexOf(expr);
-            return index != -1 ? (T) args[index] : null;
+                int index = getArgs().indexOf(expr);
+                return index != -1 ? (T) args[index] : null;
             }
 
-        @Override
+            @Override
             public Object[] toArray() {
-            return args;
+                return args;
             }
 
-    };
+        };
     }
 
 }
