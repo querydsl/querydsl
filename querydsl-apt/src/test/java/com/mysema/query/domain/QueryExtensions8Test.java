d84e2bb749917090708eb063cf8ac6a905606e70
Timo Westkämper <timo.westkamper@mysema.com>
2010-07-16 00:52:40 +0000

    organized imports

--- querydsl-apt/src/test/java/com/mysema/query/domain/QueryExtensions8Test.java
+++ querydsl-apt/src/test/java/com/mysema/query/domain/QueryExtensions8Test.java
@@ -1,7 +1,7 @@
 package com.mysema.query.domain;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.sql.Date;
 
