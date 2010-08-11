5895a2dfc47979e396608a914509daa4a1b4506a
Timo Westkämper <timo.westkamper@mysema.com>
2010-08-11 19:49:38 +0000

    organized imports

--- querydsl-collections/src/test/java/com/mysema/query/collections/OrderTest.java
+++ querydsl-collections/src/test/java/com/mysema/query/collections/OrderTest.java
@@ -10,7 +10,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
