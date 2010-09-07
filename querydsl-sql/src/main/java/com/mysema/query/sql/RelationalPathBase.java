7c7750689ad3a46be70c14906ccefe7efae7d263
Timo Westkämper <timo.westkamper@mysema.com>
2010-09-07 05:08:53 +0000

    added base class for RelationalPathBase

---
+++ querydsl-sql/src/main/java/com/mysema/query/sql/RelationalPathBase.java
@@ -0,0 +1,77 @@
+package com.mysema.query.sql;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import com.mysema.query.types.Expr;
+import com.mysema.query.types.Path;
+import com.mysema.query.types.PathMetadata;
+import com.mysema.query.types.path.BeanPath;
+
+/**
+ * @author tiwe
+ *
+ * @param <T>
+ */
+public class RelationalPathBase<T> extends BeanPath<T> implements RelationalPath<T> {
+    
+    private static final long serialVersionUID = -7031357250283629202L;
+    
+    @Nullable
+    private PrimaryKey<T> primaryKey;
+    
+    private final List<Expr<?>> columns = new ArrayList<Expr<?>>();
+    
+    private final List<ForeignKey<?>> foreignKeys = new ArrayList<ForeignKey<?>>();
+    
+    private final List<ForeignKey<?>> inverseForeignKeys = new ArrayList<ForeignKey<?>>();
+
+    public RelationalPathBase(Class<? extends T> type, String variable) {
+        super(type, variable);
+    }
+
+    public RelationalPathBase(Class<? extends T> type, PathMetadata<?> metadata) {
+        super(type, metadata);
+    }
+
+    protected PrimaryKey<T> createPrimaryKey(Path<?>... columns){
+        primaryKey = new PrimaryKey<T>(this, columns);
+        return primaryKey;
+    }
+    
+    protected <F> ForeignKey<F> createForeignKey(Path<?> local, String foreign){
+        ForeignKey<F> foreignKey = new ForeignKey<F>(this, local, foreign);
+        foreignKeys.add(foreignKey);
+        return foreignKey;
+    }
+    
+    protected <F> ForeignKey<F> createForeignKey(List<? extends Path<?>> local, List<String> foreign){
+        ForeignKey<F> foreignKey = new ForeignKey<F>(this, local, foreign);
+        foreignKeys.add(foreignKey);
+        return foreignKey;
+    }
+    
+    @Override
+    public List<Expr<?>> getColumns() {
+        return columns;
+    }
+
+    @Override
+    public Collection<ForeignKey<?>> getForeignKeys() {
+        return foreignKeys;
+    }
+
+    @Override
+    public Collection<ForeignKey<?>> getInverseForeignKeys() {
+        return inverseForeignKeys;
+    }
+
+    @Override
+    public PrimaryKey<T> getPrimaryKey() {
+        return primaryKey;
+    }
+
+}
