56d7b86c6e8faa6d6ea8a292562c53ab8dc48b65
Timo Westkämper <timo.westkamper@mysema.com>
2010-09-09 11:11:44 +0000

    added javadocs

--- querydsl-sql/src/main/java/com/mysema/query/sql/Wildcard.java
+++ querydsl-sql/src/main/java/com/mysema/query/sql/Wildcard.java
@@ -1,18 +1,27 @@
+/*
+ * Copyright (c) 2010 Mysema Ltd.
+ * All rights reserved.
+ *
+ */
 package com.mysema.query.sql;
 
 import com.mysema.query.types.Expr;
 import com.mysema.query.types.custom.CSimple;
 import com.mysema.query.types.expr.ENumber;
 
+/**
+ * Wildcard provides shortcuts for the wildcard (*) and wildcard count (count(*)) expressions
+ * 
+ * @author sasa
+ *
+ */
 public final class Wildcard {
 
     private static final long serialVersionUID = -675749944676437551L;
 
     public static final Expr<Object[]> all = CSimple.create(Object[].class, "*");
 
-    private Wildcard() {
-        // Not allowed
-    }
+    private Wildcard() {}
     
     public static ENumber<Long> count() {
         return all.count();
