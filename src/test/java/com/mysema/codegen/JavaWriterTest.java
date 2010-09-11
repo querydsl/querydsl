63bb4a77eb4e8c17167806448674e9d671d39d0f
Timo Westkämper <timo.westkamper@mysema.com>
2010-09-11 16:12:33 +0000

    updated version to 0.2.5

--- src/test/java/com/mysema/codegen/JavaWriterTest.java
+++ src/test/java/com/mysema/codegen/JavaWriterTest.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.lang.annotation.Annotation;
 import java.util.Arrays;
-import java.util.Collections;
 
 import org.apache.commons.collections15.Transformer;
 import org.apache.commons.io.IOUtils;
