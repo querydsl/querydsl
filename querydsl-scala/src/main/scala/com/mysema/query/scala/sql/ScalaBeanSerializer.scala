0b3e732866590ca5c8208323960d486c34f5ce70
Vesa Martilla <vesa.marttila@mysema.com>
2010-09-22 09:02:07 +0000

    Replaced for-loops with more Scala-esque usage of map and flatMap.

--- querydsl-scala/src/main/scala/com/mysema/query/scala/sql/ScalaBeanSerializer.scala
+++ querydsl-scala/src/main/scala/com/mysema/query/scala/sql/ScalaBeanSerializer.scala
@@ -64,14 +64,17 @@ class ScalaBeanSerializer extends Serializer {
 
     def getAnnotationTypes(model: EntityType): Set[String] = {
         var imports = new HashSet[String]();
-        for (annotation <- model.getAnnotations){
-            imports.add(annotation.annotationType.getName);
-        }
-        for (property <- model.getProperties){
-            for (annotation <- property.getAnnotations){
-                imports.add(annotation.annotationType.getName);
-            }
-        }
+//        for (annotation <- model.getAnnotations){
+//            imports.add(annotation.annotationType.getName);
+//        }
+        imports.addAll(model.getAnnotations.map(_.annotationType.getName))
+        // flatMap flattens the results of the map-function.
+        // E.g. List(List(1,2,3), List(4,5,6)).flatMap(_.map(_*3)) ends up as List(3, 6, 9, 12, 15, 18).
+        imports.addAll(model.getProperties.flatMap(_.getAnnotations.map(_.annotationType.getName)))
+        
+//        for (property <- model.getProperties; annotation <- property.getAnnotations) {
+//            imports.add(annotation.annotationType.getName);
+//        }
         imports;
     }
     
