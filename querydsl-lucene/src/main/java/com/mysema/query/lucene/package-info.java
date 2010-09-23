e347f1c63d5dab54fd435669ff932079087c795c
Timo Westkämper <timo.westkamper@mysema.com>
2010-09-23 08:39:09 +0000

    added package-info with FindBugs defaults

---
+++ querydsl-lucene/src/main/java/com/mysema/query/lucene/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2009 Mysema Ltd.
+ * All rights reserved.
+ *
+ */
+
+@DefaultAnnotation( { Nonnull.class })
+package com.mysema.query.lucene;
+
+import javax.annotation.Nonnull;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+
