35996c4bbfc4fbe5eadc577f6ad3e6b3abe42fe7
Timo Westkämper <timo.westkamper@mysema.com>
2010-10-26 09:33:20 +0000

    added tests for mappings

--- querydsl-sql/src/test/java/com/mysema/query/sql/JDBCTypeMappingTest.java
+++ querydsl-sql/src/test/java/com/mysema/query/sql/JDBCTypeMappingTest.java
@@ -1,7 +1,8 @@
 package com.mysema.query.sql;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.sql.Blob;
 import java.sql.Types;
 
 import org.junit.Test;
@@ -13,6 +14,29 @@ public class JDBCTypeMappingTest {
         JDBCTypeMapping typeMapping = new JDBCTypeMapping();
         assertEquals(Float.class, typeMapping.get(Types.FLOAT));
         assertEquals(Float.class, typeMapping.get(Types.REAL));
+    }
+    
+    @Test
+    public void StringTypes(){
+        JDBCTypeMapping typeMapping = new JDBCTypeMapping();
+        assertEquals(String.class, typeMapping.get(Types.CHAR));
+        assertEquals(String.class, typeMapping.get(Types.NCHAR));
+        assertEquals(String.class, typeMapping.get(Types.CLOB));
+        assertEquals(String.class, typeMapping.get(Types.NCLOB));
+        assertEquals(String.class, typeMapping.get(Types.LONGVARCHAR));
+        assertEquals(String.class, typeMapping.get(Types.LONGNVARCHAR));
+        assertEquals(String.class, typeMapping.get(Types.SQLXML));
+        assertEquals(String.class, typeMapping.get(Types.VARCHAR));
+        assertEquals(String.class, typeMapping.get(Types.NVARCHAR));
+    }
+    
+    @Test
+    public void BlobTypes(){
+        JDBCTypeMapping typeMapping = new JDBCTypeMapping();
+        assertEquals(Blob.class, typeMapping.get(Types.BLOB));
+        assertEquals(Blob.class, typeMapping.get(Types.BINARY));
+        assertEquals(Blob.class, typeMapping.get(Types.VARBINARY));
+        assertEquals(Blob.class, typeMapping.get(Types.LONGVARBINARY));
     }
 
 }
