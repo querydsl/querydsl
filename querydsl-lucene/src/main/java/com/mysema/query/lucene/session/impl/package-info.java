4437ee8a87305e79c8b97f928a3d03cb04aaaac8
Timo Westkämper <timo.westkamper@mysema.com>
2010-12-30 08:42:15 +0000

    added package-infos

---
+++ querydsl-lucene/src/main/java/com/mysema/query/lucene/session/impl/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2010 Mysema Ltd.
+ * All rights reserved.
+ *
+ */
+
+/**
+ * Contains basic Query elements
+ */
+@DefaultAnnotation( { Nonnull.class })
+package com.mysema.query.lucene.session.impl;
+
+import javax.annotation.Nonnull;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+
