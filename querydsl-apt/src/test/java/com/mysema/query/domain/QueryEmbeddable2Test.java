e1cc89d53a150ce4c47a22b230ee7d1edd0908c0
Timo Westkämper <timo.westkamper@mysema.com>
2011-01-13 08:25:00 +0000

    added further tests

---
+++ querydsl-apt/src/test/java/com/mysema/query/domain/QueryEmbeddable2Test.java
@@ -0,0 +1,35 @@
+package com.mysema.query.domain;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import com.mysema.query.annotations.QueryEmbeddable;
+import com.mysema.query.annotations.QueryEntity;
+
+public class QueryEmbeddable2Test {
+
+    @QueryEntity
+    public class User {
+    
+        Complex<String> complex;
+    }
+    
+    @QueryEmbeddable
+    public class Complex<T extends Comparable<T>> implements Comparable<Complex<T>> {
+
+        T a;
+        
+        @Override
+        public int compareTo(Complex<T> arg0) {
+            return 0;
+        }
+        
+    }
+    
+    @Test
+    public void User_Complex_a() {
+        assertNotNull(QQueryEmbeddable2Test_User.user.complex.a);
+    }
+    
+}
