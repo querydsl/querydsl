46f368136f25fddda61d7a99f4024d69f1516683
Timo Westkämper <timo.westkamper@mysema.com>
2011-04-27 07:11:37 +0000

    fixed hibernate apt one jar config

--- querydsl-jpa/src/test/java/com/mysema/query/PackageVerification.java
+++ querydsl-jpa/src/test/java/com/mysema/query/PackageVerification.java
@@ -1,6 +1,7 @@
 package com.mysema.query;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
