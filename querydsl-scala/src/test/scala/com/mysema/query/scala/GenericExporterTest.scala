0c9609a13c4f75cd52b96f3d6ed5d0cc388be0c5
Vesa Martilla <vesa.marttila@mysema.com>
2011-05-06 07:48:40 +0000

    GenericExporterTest: Formatting.

--- querydsl-scala/src/test/scala/com/mysema/query/scala/GenericExporterTest.scala
+++ querydsl-scala/src/test/scala/com/mysema/query/scala/GenericExporterTest.scala
@@ -1,10 +1,12 @@
 package com.mysema.query.scala;
+
 import com.mysema.query.annotations._
 import com.mysema.query.codegen.GenericExporter
 import org.junit.Test
+import io.Source.fromFile
 
 class GenericExporterTest extends CompileTestUtils {
-    
+
   @Test
   def Export() {
     val exporter = new GenericExporter()
@@ -12,9 +14,12 @@ class GenericExporterTest extends CompileTestUtils {
     exporter.setSerializerClass(classOf[ScalaEntitySerializer])
     exporter.setCreateScalaSources(true)
     exporter.export(getClass.getPackage)
-    
-    val targetFolder = new java.io.File("target/gen1/com/mysema/query/scala/") 
-    val sources = targetFolder listFiles() filter( _.getName.endsWith(".scala") ) map( io.Source.fromFile(_).mkString ) mkString("\n")
+
+    val targetFolder = new java.io.File("target/gen1/com/mysema/query/scala/")
+    val sources = (targetFolder listFiles ()
+      filter (_.getName.endsWith(".scala"))
+      map (fromFile(_).mkString)
+      mkString ("\n"))
     assertCompileSuccess(sources)
   }
 
@@ -22,36 +27,36 @@ class GenericExporterTest extends CompileTestUtils {
 
 @QuerySupertype
 class Superclass {
-    
+
   var id: Int = _
-    
+
 }
 
 @QueryEntity
 class EntityClass extends Superclass {
-    
-    // FIXME
-//  var comparable: Comparable[_] = _   
-    
-  var code: String = _  
+
+  // FIXME
+  //  var comparable: Comparable[_] = _   
+
+  var code: String = _
 
   var stringList: java.util.List[String] = _
-  
+
   var embedded: EmbeddableClass = _
-  
+
 }
 
 @QueryEntity
 class EntitySubclass extends EntityClass {
- 
-  var property: String = _ 
-    
+
+  var property: String = _
+
 }
 
 @QueryEmbeddable
 class EmbeddableClass {
 
-  var property: String = _  
-    
+  var property: String = _
+
 }
 
