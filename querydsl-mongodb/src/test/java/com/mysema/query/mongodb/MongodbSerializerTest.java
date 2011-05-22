920ee3246ed0032709a46a7eb8f6741fc4379ecc
Timo Westkämper <timo.westkamper@mysema.com>
2011-05-22 08:30:16 +0000

    #785511 : fixed between handling

--- querydsl-mongodb/src/test/java/com/mysema/query/mongodb/MongodbSerializerTest.java
+++ querydsl-mongodb/src/test/java/com/mysema/query/mongodb/MongodbSerializerTest.java
@@ -139,7 +139,7 @@ public class MongodbSerializerTest {
 
         assertQuery(
                 year.between(1, 10),
-                dbo("year", dbo("$goe", 1).append("$loe", 10))
+                dbo("year", dbo("$gte", 1).append("$lte", 10))
         );
     }
 
