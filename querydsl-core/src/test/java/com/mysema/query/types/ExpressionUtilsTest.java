ecf6f052878d04e3212eb27b3b1666f7da4824bd
Timo Westkämper <timo.westkamper@mysema.com>
2011-08-09 10:41:52 +0300

    added tests for ExpressionUtils

--- querydsl-core/src/test/java/com/mysema/query/types/ExpressionUtilsTest.java
+++ querydsl-core/src/test/java/com/mysema/query/types/ExpressionUtilsTest.java
@@ -1,6 +1,8 @@
 package com.mysema.query.types;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -8,6 +10,10 @@ import com.mysema.query.types.path.StringPath;
 
 public class ExpressionUtilsTest {
 
+    private static final StringPath str = new StringPath("str");
+    
+    private static final StringPath str2 = new StringPath("str2");
+    
     @Test
     public void LikeToRegex() {
         assertEquals(".*", regex(ConstantImpl.create("%")));
@@ -38,6 +44,46 @@ public class ExpressionUtilsTest {
     
     private String like(Expression<String> expr){
         return ExpressionUtils.regexToLike(expr).toString();
+    }
+    
+    @Test
+    public void Count() {
+        assertEquals("count(str)", ExpressionUtils.count(str).toString());
+    }
+    
+    @Test
+    public void EqConst() {
+        assertEquals("str = X", ExpressionUtils.eqConst(str, "X").toString());
+    }
+    
+    @Test
+    public void Eq() {
+        assertEquals("str = str2", ExpressionUtils.eq(str, str2).toString());    
+    }
+    
+    @Test
+    public void In() {
+        assertEquals("str in [a, b, c]", ExpressionUtils.in(str, Arrays.asList("a","b","c")).toString());
+    }
+    
+    @Test
+    public void IsNull() {
+        assertEquals("str is null", ExpressionUtils.isNull(str).toString());
+    }
+    
+    @Test
+    public void IsNotNull() {
+        assertEquals("str is not null", ExpressionUtils.isNotNull(str).toString());
+    }
+    
+    @Test
+    public void NeConst() {
+        assertEquals("str != X", ExpressionUtils.neConst(str, "X").toString());
+    }
+    
+    @Test
+    public void Ne() {
+        assertEquals("str != str2", ExpressionUtils.ne(str, str2).toString());
     }
 
 }
