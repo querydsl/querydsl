0a19471307ebdb203968e4ca6b60984bb7786b17
Timo Westkämper <timo.westkamper@mysema.com>
2011-09-06 08:59:24 +0300

    #842360 : made Query extend SimpleQuery

--- querydsl-core/src/main/java/com/mysema/query/Query.java
+++ querydsl-core/src/main/java/com/mysema/query/Query.java
@@ -5,11 +5,7 @@
  */
 package com.mysema.query;
 
-import javax.annotation.Nonnegative;
-
 import com.mysema.query.types.Expression;
-import com.mysema.query.types.OrderSpecifier;
-import com.mysema.query.types.ParamExpression;
 import com.mysema.query.types.Predicate;
 
 /**
@@ -21,7 +17,7 @@ import com.mysema.query.types.Predicate;
  * @author tiwe
  * @see SimpleQuery
  */
-public interface Query<Q extends Query<Q>> extends FilteredClause<Q>{
+public interface Query<Q extends Query<Q>> extends SimpleQuery<Q>{
 
     /**
      * Defines the grouping/aggregation expressions
@@ -38,54 +34,5 @@ public interface Query<Q extends Query<Q>> extends FilteredClause<Q>{
      * @return
      */
     Q having(Predicate... o);
-
-    /**
-     * Defines the order expressions
-     *
-     * @param o
-     * @return
-     */
-    Q orderBy(OrderSpecifier<?>... o);
-
-    /**
-     * Defines the limit / max results for the query results
-     *
-     * @param limit
-     * @return
-     */
-    Q limit(@Nonnegative long limit);
-
-    /**
-     * Defines the offset for the query results
-     *
-     * @param offset
-     * @return
-     */
-    Q offset(@Nonnegative long offset);
-
-    /**
-     * Defines both limit and offset of the query results
-     *
-     * @param mod
-     * @return
-     */
-    Q restrict(QueryModifiers mod);
-
-    /**
-     * Set the given parameter to the given value
-     *
-     * @param <T>
-     * @param param
-     * @param value
-     * @return
-     */
-    <T> Q set(ParamExpression<T> param, T value);
-    
-    /**
-     * Set the Query to return distinct results
-     * 
-     * @return
-     */
-    Q distinct();
-
+   
 }
