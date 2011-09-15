5927fe23c95e8d3d6ba91373493f8969904619af
Samppa Saarela <samppa.saarela@mysema.com>
2011-09-15 09:49:46 +0300

    Tested nested transformers

--- querydsl-core/src/test/java/com/mysema/query/support/GroupByTest.java
+++ querydsl-core/src/test/java/com/mysema/query/support/GroupByTest.java
@@ -3,16 +3,21 @@ package com.mysema.query.support;
 
 import static junit.framework.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 
 import com.mysema.commons.lang.CloseableIterator;
 import com.mysema.commons.lang.EmptyCloseableIterator;
 import com.mysema.commons.lang.IteratorAdapter;
+import com.mysema.query.Projectable;
+import com.mysema.query.ResultTransformer;
 import com.mysema.query.Tuple;
 import com.mysema.query.types.Expression;
 import com.mysema.query.types.expr.NumberExpression;
@@ -28,6 +33,17 @@ public class GroupByTest {
 
     private final NumberExpression<Integer> commentId = new NumberPath<Integer>(Integer.class, null, "commentId");
 
+    static class PostWithComments {
+        public Integer id;
+        public String name;
+        public Set<Integer> comments;
+        public PostWithComments(Integer id, String name, Set<Integer> comments) {
+            this.id = id;
+            this.name = name;
+            this.comments = comments;
+        }
+    }
+    
     @Test
     public void Expression_Order() {
         new GroupBy(postId, postName, commentId).transform(new AbstractProjectable(){
@@ -48,20 +64,18 @@ public class GroupByTest {
      */
     @Test
     public void Multiple_Groups() {
-        Collection<Group> results = new GroupBy(postId, postName, commentId).transform(new AbstractProjectable(){
-            public CloseableIterator<Object[]> iterate(Expression<?>[] args) {
-                return iterator(
-                        row(1, "post 1", 1),
-                        row(2, "post 2", 4),
-                        row(1, "post 1", 2),
-                        row(2, "post 2", 5),
-                        row(3, "post 3", 6),
-                        row(null, "null post", 7),
-                        row(null, "null post", 8),
-                        row(1, "post 1", 3)
-                );
-            }
-        });
+        Collection<Group> results = new GroupBy(postId, postName, commentId).transform(
+            projectable(
+                row(1, "post 1", 1),
+                row(2, "post 2", 4),
+                row(1, "post 1", 2),
+                row(2, "post 2", 5),
+                row(3, "post 3", 6),
+                row(null, "null post", 7),
+                row(null, "null post", 8),
+                row(1, "post 1", 3)
+            )
+        );
         assertEquals(4, results.size());
         Iterator<Group> iter = results.iterator();
         
@@ -101,14 +115,10 @@ public class GroupByTest {
     
     @Test
     public void Get_Row() {
-        Collection<Group> results = new GroupBy(postId, postName, commentId).transform(new AbstractProjectable(){
-            public CloseableIterator<Object[]> iterate(Expression<?>[] args) {
-                return iterator(
-                        row(1, "post 1", 1),
-                        row(null, "null post", 2)
-                );
-            }
-        });
+        Collection<Group> results = new GroupBy(postId, postName, commentId).transform(projectable(
+            row(1, "post 1", 1),
+            row(null, "null post", 2)
+        ));
         assertEquals(2, results.size());
         Iterator<Group> iter = results.iterator();
         
@@ -125,12 +135,52 @@ public class GroupByTest {
     
     @Test(expected=IndexOutOfBoundsException.class)
     public void Row_Out_of_Bounds() {
-        Collection<Group> results = new GroupBy(postId, postName, commentId).transform(new AbstractProjectable(){
-            public CloseableIterator<Object[]> iterate(Expression<?>[] args) {
-                return iterator(row(1, "post 1", 1));
-            }
-        });
+        Collection<Group> results = new GroupBy(postId, postName, commentId).transform(projectable(
+            row(1, "post 1", 1)
+        ));
         results.iterator().next().getRow(1);
+    }
+    
+    @Test
+    public void Nested_Transformers() {
+        // NOTE: This kind of bean projecting transformer can be generalized...
+        
+        // A transformer that uses results of GroupBy transformer to instantiate beans with collections
+        List<PostWithComments> posts = new ResultTransformer<List<PostWithComments>>() {
+            
+            @Override
+            public List<PostWithComments> transform(Projectable projectable) {
+                // Nested transformer
+                Collection<Group> groups = new GroupBy(postId, postName, commentId).transform(projectable);
+                
+                List<PostWithComments> results = new ArrayList<GroupByTest.PostWithComments>(groups.size());
+                for (Group group : groups) {
+                    results.add( new PostWithComments(group.get(postId), group.get(postName), new HashSet<Integer>(group.getList(commentId))) );
+                }
+                return results;
+            }
+
+        }.transform(projectable(
+            row(1, "post 1", 1),
+            row(2, "post 2", 4),
+            row(1, "post 1", 2),
+            row(2, "post 2", 5),
+            row(3, "post 3", 6)
+        ));
+        
+        assertEquals(3, posts.size());
+        PostWithComments post = posts.get(0);
+        assertEquals(toInt(1), post.id);
+        assertEquals("post 1", post.name);
+        assertEquals(new HashSet<Integer>(Arrays.asList(toInt(1), toInt(2))), post.comments);
+    }
+    
+    private Projectable projectable(final Object[]... rows) {
+        return new AbstractProjectable(){
+            public CloseableIterator<Object[]> iterate(Expression<?>[] args) {
+                return iterator(rows);
+            }
+        };
     }
     
     private Integer toInt(int i) {
