428e16d981467bd91c28c6da5c960ebd567131a0
Timo Westkämper <timo.westkamper@mysema.com>
2011-11-07 20:17:16 +0200

    #40 : fixed commons:io version

---
+++ querydsl-sql/src/test/java/com/mysema/query/sql/RelationalFunctionCallTest.java
@@ -0,0 +1,28 @@
+package com.mysema.query.sql;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.mysema.query.types.ConstantImpl;
+import com.mysema.query.types.path.StringPath;
+
+public class RelationalFunctionCallTest {
+    
+    @Test
+    public void NoArgs() {
+        RelationalFunctionCall<String> functionCall = RelationalFunctionCall.create(String.class, "getElements");
+        assertEquals("getElements()", functionCall.getTemplate().toString());
+    }
+    
+    @Test
+    public void TwoArgs() {
+        StringPath str = new StringPath("str");
+        RelationalFunctionCall<String> functionCall = RelationalFunctionCall.create(String.class, "getElements", "a", str);
+        assertEquals("getElements({0}, {1})", functionCall.getTemplate().toString());
+        assertEquals(ConstantImpl.create("a"), functionCall.getArg(0));
+        assertEquals(str, functionCall.getArg(1));
+        
+    }
+
+}
