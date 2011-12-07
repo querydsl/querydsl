6f761fec54d5254d48e110fd06ae11f24d6692b8
Timo Westkämper <timo.westkamper@mysema.com>
2011-12-07 16:24:09 +0200

    replaced mutable Collections with builders in GroupBy

--- querydsl-scala/src/main/scala/com/mysema/query/scala/GroupBy.scala
+++ querydsl-scala/src/main/scala/com/mysema/query/scala/GroupBy.scala
@@ -8,9 +8,6 @@ package com.mysema.query.scala;
 import com.mysema.query.types._
 import com.mysema.query.group._
 
-import scala.collection.immutable.{ List, Set, Map }
-import scala.collection.mutable.{ ListBuffer, Set => MSet, Map => MMap }
-
 object GroupBy extends GroupBy
 
 /**
@@ -25,25 +22,25 @@ trait GroupBy {
   
   def set[T](e: Ex[T]) = new AbstractGroupExpression[T,Set[T]](classOf[Set[T]], e) {
     def createGroupCollector() : GroupCollector[T,Set[T]] = new GroupCollector[T, Set[T]]() {
-      val set = MSet[T]()
+      val set = Set.newBuilder[T]
       def add(o: T): Unit = set.+=(o) 
-      def get(): Set[T] = set.toSet
+      def get(): Set[T] = set.result
     }  
   }
     
   def list[T](e: Ex[T]) = new AbstractGroupExpression[T,List[T]](classOf[List[T]], e) {
     def createGroupCollector(): GroupCollector[T,List[T]] = new GroupCollector[T, List[T]]() {
-      val list = new ListBuffer[T]()
+      val list = List.newBuilder[T]
       def add(o: T): Unit = list.+=(o) 
-      def get(): List[T] = list.toList
+      def get(): List[T] = list.result
     }  
   }
       
   def map[K,V](k: Ex[K], v: Ex[V]) = new AbstractGroupExpression[(K,V),Map[K,V]](classOf[Map[K,V]], new Tu2Ex(k,v)) {
     def createGroupCollector(): GroupCollector[(K,V),Map[K,V]] = new GroupCollector[(K,V),Map[K,V]]() {
-      val map = MMap[K,V]()
+      val map = Map.newBuilder[K,V]
       def add(tu: (K,V)) = map.+=(tu)
-      def get(): Map[K,V] = map.toMap
+      def get(): Map[K,V] = map.result
     }        
   }
     
