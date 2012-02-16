e44b1207e26951c0899b6f625417aaac0dc86ada
Timo Westkämper <timo.westkamper@mysema.com>
2012-02-16 21:59:23 +0200

    added oracle JPA tests

---
+++ querydsl-jpa/src/test/java/com/mysema/query/_oracle/OracleStandardTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2011, Mysema Ltd
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mysema.query._oracle;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+
+import com.mysema.query.AbstractHibernateTest;
+import com.mysema.query.Target;
+import com.mysema.testutil.HibernateConfig;
+import com.mysema.testutil.HibernateTestRunner;
+
+@Ignore // FIXME
+@RunWith(HibernateTestRunner.class)
+@HibernateConfig("oracle.properties")
+public class OracleStandardTest extends AbstractHibernateTest{
+
+    @Override
+    protected Target getTarget() {
+        return Target.ORACLE;
+    }
+
+}
