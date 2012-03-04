0777ba31ce1d2464f6503d474c22931620a16269
Timo Westkämper <timo.westkamper@mysema.com>
2012-03-04 21:30:30 +0200

    added codegen tests

---
+++ querydsl-core/src/test/java/com/mysema/query/codegen/Examples.java
@@ -0,0 +1,28 @@
+package com.mysema.query.codegen;
+
+import com.mysema.query.annotations.QueryEntity;
+
+public class Examples {
+    
+    @QueryEntity
+    public static class SimpleEntity {
+
+    }
+
+    @QueryEntity
+    public static abstract class AbstractEntity<Id extends java.io.Serializable> {
+
+      Id id;
+
+      String first;
+
+    }
+
+    @QueryEntity
+    public static class SubEntity extends AbstractEntity<java.lang.Long> {
+     
+      String second;
+     
+    } 
+
+}
