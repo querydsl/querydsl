8fa39026d42730c42b59800ac61160e60f280d41
Timo Westkämper <timo.westkamper@mysema.com>
2012-12-17 09:42:02 +0200

    fix validation test

--- querydsl-sql/src/test/java/com/mysema/query/sql/JoinUsageTest.java
+++ querydsl-sql/src/test/java/com/mysema/query/sql/JoinUsageTest.java
@@ -15,13 +15,14 @@ package com.mysema.query.sql;
 
 import org.junit.Test;
 
+import com.mysema.query.DefaultQueryMetadata;
 import com.mysema.query.sql.domain.QSurvey;
 
 public class JoinUsageTest {
     
     @Test(expected=IllegalStateException.class)
     public void Join_Already_Declared(){
-        SQLSubQuery subQuery = new SQLSubQuery();
+        SQLSubQuery subQuery = new SQLSubQuery(new DefaultQueryMetadata());
         subQuery.from(QSurvey.survey).fullJoin(QSurvey.survey);
     }
 
