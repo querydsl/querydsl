61f1aa4614da9110340e1883c4190d0816b61591
Timo Westkämper <timo.westkamper@mysema.com>
2012-12-19 10:40:12 +0200

    add copyright headers

--- src/main/java/com/mysema/codegen/EvaluatorFactory.java
+++ src/main/java/com/mysema/codegen/EvaluatorFactory.java
@@ -1,6 +1,15 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2010, Mysema Ltd
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.mysema.codegen;
 
@@ -14,7 +23,16 @@ import java.util.Map;
  */
 public interface EvaluatorFactory {
 
-    <T> Evaluator<T> createEvaluator(String source, Class<? extends T> projectionType, String[] names, Class<?>[] classes, Map<String, Object> constants);
+    /**
+     * @param source
+     * @param projectionType
+     * @param names
+     * @param classes
+     * @param constants
+     * @return
+     */
+    <T> Evaluator<T> createEvaluator(String source, Class<? extends T> projectionType, 
+            String[] names, Class<?>[] classes, Map<String, Object> constants);
 
     /**
      * Create a new Evaluator instance
@@ -33,6 +51,7 @@ public interface EvaluatorFactory {
      * @return
      */
     @SuppressWarnings(value = "unchecked")
-    <T> Evaluator<T> createEvaluator(String source, ClassType projection, String[] names, Type[] types, Class<?>[] classes, Map<String, Object> constants);
+    <T> Evaluator<T> createEvaluator(String source, ClassType projection, String[] names, 
+            Type[] types, Class<?>[] classes, Map<String, Object> constants);
     
 }
