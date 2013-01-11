d3f1a51187be1729c4299548653f9215642578f8
Timo Westkämper <timo.westkamper@mysema.com>
2013-01-11 21:00:43 +0200

    #318 Fix Roo tests

--- querydsl-apt/src/test/java/com/mysema/query/domain/RooEntities.java
+++ querydsl-apt/src/test/java/com/mysema/query/domain/RooEntities.java
@@ -3,11 +3,11 @@ package com.mysema.query.domain;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
 
 public class RooEntities {
     
-    @RooEntity
+    @RooJpaEntity
     public static class MyEntity {
         
         @Id 
