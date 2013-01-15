f7b7d8f86b02f1a67f7513c918a70d1d7b2bf72c
Timo Westkämper <timo.westkamper@mysema.com>
2013-01-15 16:38:36 +0200

    Improve dir creation

--- querydsl-root/dist.sh
+++ querydsl-root/dist.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf target/dist
-mkdir target/dist
+mkdir -p target/dist
 
 echo "Creating javadocs"
 mvn javadoc:aggregate
@@ -15,7 +15,7 @@ done
 echo "Creating reference documentation"
 cd ../querydsl-docs
 mvn -Dxslthl.config=http://docbook.sourceforge.net/release/xsl/current/highlighting/xslthl-config.xml clean package
-mkdir ../querydsl-root/target/dist/reference
+mkdir -p ../querydsl-root/target/dist/reference
 cp -R target/docbook/publish/en-US/* ../querydsl-root/target/dist/reference/
 cd ../querydsl-root
 
