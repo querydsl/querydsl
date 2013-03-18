9d790c585da920c150ff164e9e8c74a5551b3064
Timo Westkämper <timo.westkamper@mysema.com>
2013-03-18 12:13:12 +0200

    #368 Add test

---
+++ querydsl-apt/src/test/java/com/mysema/query/domain/Generic10Test.java
@@ -0,0 +1,35 @@
+package com.mysema.query.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
+
+import org.junit.Ignore;
+
+@Ignore
+public class Generic10Test {
+    
+    public interface Tradable {}
+
+    public interface Market<T> {}
+    
+    @Entity
+    public static class FutureTrade implements Tradable {}
+
+    @MappedSuperclass
+    public static abstract class AbstractTradingMarket<T extends Tradable> implements Market<T>{
+
+        @OneToOne
+        private TradingMarketLedger<AbstractTradingMarket<T>> ledger;
+    }
+
+    @Entity
+    public static abstract class AbstractFuturesMarket extends AbstractTradingMarket<FutureTrade> {}
+
+    @Entity
+    public static class CommonFuturesMarket extends AbstractFuturesMarket {}
+
+    @Entity
+    public static class TradingMarketLedger<M extends Market<? extends Tradable>> {}
+    
+}
