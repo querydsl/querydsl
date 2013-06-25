9cf742d647dd7d04d4ae5c0382aaaa30e8172825
Timo Westkämper <timo.westkamper@mysema.com>
2013-06-25 22:47:04 +0300

    Comment MSSQLSuiteTest out

--- querydsl-sql/src/test/java/com/mysema/query/suites/MSSQLSuiteTest.java
+++ querydsl-sql/src/test/java/com/mysema/query/suites/MSSQLSuiteTest.java
@@ -1,10 +1,12 @@
 package com.mysema.query.suites;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 
 import com.mysema.query.Connections;
 import com.mysema.query.sql.SQLServer2005Templates;
 
+@Ignore
 public class MSSQLSuiteTest extends AbstractSuite {
 
     @BeforeClass
