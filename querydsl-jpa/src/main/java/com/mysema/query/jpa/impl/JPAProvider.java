90fdadf71e96406cf1643655d6385147ae084d88
Timo Westkämper <timo.westkamper@mysema.com>
2013-07-07 21:11:52 +0300

    #440 Extend JPAProvider detection

--- querydsl-jpa/src/main/java/com/mysema/query/jpa/impl/JPAProvider.java
+++ querydsl-jpa/src/main/java/com/mysema/query/jpa/impl/JPAProvider.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2012, Mysema Ltd
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -27,14 +27,16 @@ import com.mysema.query.jpa.OpenJPATemplates;
 
 /**
  * JPAProvider provides detection of the JPA provider based on the EntityManager instance
- * 
+ *
  * @author tiwe
  *
  */
 public final class JPAProvider {
-    
+
     private static final Map<Class<?>, JPQLTemplates> mappings = Maps.newHashMap();
-    
+
+    private static final Map<String, JPQLTemplates> templatesByName = Maps.newHashMap();
+
     private static void addMapping(String className, JPQLTemplates templates) {
         try {
             mappings.put(Class.forName(className), templates);
@@ -50,17 +52,33 @@ public final class JPAProvider {
         addMapping("org.datanucleus.jpa.EntityManagerImpl", DataNucleusTemplates.DEFAULT);
         addMapping("org.datanucleus.ObjectManager", DataNucleusTemplates.DEFAULT);
         addMapping("org.datanucleus.ObjectManagerImpl", DataNucleusTemplates.DEFAULT);
+
+        templatesByName.put("batoo", BatooTemplates.DEFAULT);
+        templatesByName.put("eclipselink", EclipseLinkTemplates.DEFAULT);
+        templatesByName.put("hibernate", HQLTemplates.DEFAULT);
+        templatesByName.put("openjpa", OpenJPATemplates.DEFAULT);
+        templatesByName.put("datanucleus", DataNucleusTemplates.DEFAULT);
     }
-    
+
     public static JPQLTemplates getTemplates(EntityManager em) {
+        // detect by delegate
         for (Map.Entry<Class<?>, JPQLTemplates> entry : mappings.entrySet()) {
             if (entry.getKey().isAssignableFrom(em.getDelegate().getClass())) {
                 return entry.getValue();
             }
         }
+        // detect by properties
+        for (String key : em.getEntityManagerFactory().getProperties().keySet()) {
+            key = key.toLowerCase();
+            for (Map.Entry<String, JPQLTemplates> entry : templatesByName.entrySet()) {
+                if (key.contains(entry.getKey())) {
+                    return entry.getValue();
+                }
+            }
+        }
         return JPQLTemplates.DEFAULT;
     }
-    
+
     private JPAProvider() {}
-    
+
 }
