021ba04dc1ec21d986d802159e259f5f76781997
Timo Westkämper <timo.westkamper@mysema.com>
2013-10-11 21:45:07 +0400

    Add includedClasses and includedPackages #517

--- querydsl-apt/src/test/java/com/mysema/query/apt/IncludedClassesTest.java
+++ querydsl-apt/src/test/java/com/mysema/query/apt/IncludedClassesTest.java
@@ -24,22 +24,22 @@ import java.util.List;
 
 import org.junit.Test;
 
-public class ExcludedPackagesTest extends AbstractProcessorTest {
+public class IncludedClassesTest extends AbstractProcessorTest {
 
     private static final String packagePath = "src/test/java/com/mysema/query/";
 
     @Test
     public void Process() throws IOException {
         List<String> classes = getFiles(packagePath);
-        process(QuerydslAnnotationProcessor.class, classes, "excludedPackages");
+        process(QuerydslAnnotationProcessor.class, classes, "includedClasses");
 
-        assertFalse(new File("target/excludedPackages/com/mysema/query/domain/p1").exists());
-        assertTrue(new File("target/excludedPackages/com/mysema/query/domain/p2").exists());
+        assertTrue(new File("target/includedClasses/com/mysema/query/domain/QArrayTest_ArrayTestEntity.java").exists());
+        assertFalse(new File("target/includedClasses/com/mysema/query/domain/QArray2Test_Example.java").exists());
     }
 
     @Override
     protected Collection<String> getAPTOptions() {
-        return Arrays.asList("-Aquerydsl.excludedPackages=com.mysema.query.domain.p1");
+        return Arrays.asList("-Aquerydsl.includedClasses=com.mysema.query.domain.ArrayTest.ArrayTestEntity");
     }
 
 }
--- querydsl-apt/src/test/java/com/mysema/query/apt/IncludedPackagesTest.java
+++ querydsl-apt/src/test/java/com/mysema/query/apt/IncludedPackagesTest.java
@@ -24,22 +24,22 @@ import java.util.List;
 
 import org.junit.Test;
 
-public class ExcludedPackagesTest extends AbstractProcessorTest {
+public class IncludedPackagesTest extends AbstractProcessorTest {
 
     private static final String packagePath = "src/test/java/com/mysema/query/";
 
     @Test
     public void Process() throws IOException {
         List<String> classes = getFiles(packagePath);
-        process(QuerydslAnnotationProcessor.class, classes, "excludedPackages");
+        process(QuerydslAnnotationProcessor.class, classes, "includedPackages");
 
-        assertFalse(new File("target/excludedPackages/com/mysema/query/domain/p1").exists());
-        assertTrue(new File("target/excludedPackages/com/mysema/query/domain/p2").exists());
+        assertFalse(new File("target/includedPackages/com/mysema/query/domain/p1").exists());
+        assertTrue(new File("target/includedPackages/com/mysema/query/domain/p2").exists());
     }
 
     @Override
     protected Collection<String> getAPTOptions() {
-        return Arrays.asList("-Aquerydsl.excludedPackages=com.mysema.query.domain.p1");
+        return Arrays.asList("-Aquerydsl.includedPackages=com.mysema.query.domain.p2");
     }
 
 }
