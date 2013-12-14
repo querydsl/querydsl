92a494cb7340a424a5e0102958f98315dad18846
Timo Westkämper <timo.westkamper@mysema.com>
2013-12-14 22:02:48 +0200

    Fix test

--- querydsl-scala/src/test/scala/com/mysema/query/scala/sql/QuerySyntaxTest.scala
+++ querydsl-scala/src/test/scala/com/mysema/query/scala/sql/QuerySyntaxTest.scala
@@ -6,7 +6,7 @@ import org.junit.Test
 import org.junit.Assert._
 
 class QuerySyntaxTest {
-  
+
   val b = new QBook("b")
   val c = new QCategory("c")
   val c1 = new QCategory("c1")
@@ -14,7 +14,7 @@ class QuerySyntaxTest {
   val department = new QDepartment("department")
   val user = new QUser("user")
   val user2 = new QUser("user2")
-  
+
   @Test
   def Path_Creation {
     assertEquals(new QCategory("c"), QCategory as "c")
@@ -22,9 +22,9 @@ class QuerySyntaxTest {
 
   @Test
   def Query_Syntax {
-    query from(c) innerJoin(b) from(c1) where (c1.name like "a%") orderBy(c.name asc) list(c)
+    query from(c) innerJoin(b) from(c1) where (c1.name like "a%") orderBy(c.name asc) //list(c)
     query from(c) innerJoin(b) list(b.id count)
-    
+
     query from (c) list (c.id, c.name)
   }
 
@@ -33,11 +33,11 @@ class QuerySyntaxTest {
     // superiorId -> id
     query from(user) innerJoin(user.superiorIdKey, user2)
   }
-  
+
   @Test
   def Key_Usage2 {
     // department -> id / company -> id
-    query from(user) innerJoin(user.departmentKey, department) innerJoin(department.companyKey, company)      
+    query from(user) innerJoin(user.departmentKey, department) innerJoin(department.companyKey, company)
   }
 
   def query() = new SQLSubQuery()
