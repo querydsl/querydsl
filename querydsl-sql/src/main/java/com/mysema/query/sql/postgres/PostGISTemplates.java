1f9d570da854d798478e71e9b5ed4c02e88a35d5
Timo Westkämper <timo.westkamper@mysema.com>
2014-02-04 23:17:27 +0200

    Improve postgis conversions #631

--- querydsl-sql/src/main/java/com/mysema/query/sql/postgres/PostGISTemplates.java
+++ querydsl-sql/src/main/java/com/mysema/query/sql/postgres/PostGISTemplates.java
@@ -42,7 +42,7 @@ public class PostGISTemplates extends PostgresTemplates {
     public PostGISTemplates(char escape, boolean quote) {
         super(escape, quote);
         addSpatialOps(true);
-        addCustomType(PGGeometryType.DEFAULT);
+        addCustomType(PGgeometryType.DEFAULT);
     }
 
 }
