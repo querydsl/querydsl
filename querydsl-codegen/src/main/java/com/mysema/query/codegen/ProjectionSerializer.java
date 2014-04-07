4aa045bdee728161b71a2dd02adce011708f431c
Timo Westkämper <timo.westkamper@mysema.com>
2014-04-07 16:31:04 +0300

    Improve projection handling

--- querydsl-codegen/src/main/java/com/mysema/query/codegen/ProjectionSerializer.java
+++ querydsl-codegen/src/main/java/com/mysema/query/codegen/ProjectionSerializer.java
@@ -13,21 +13,18 @@
  */
 package com.mysema.query.codegen;
 
-import java.io.IOException;
+import com.google.common.base.Function;
+import com.google.common.collect.Sets;
+import com.mysema.codegen.CodeWriter;
+import com.mysema.codegen.model.*;
+import com.mysema.query.types.ConstructorExpression;
+import com.mysema.query.types.Expression;
+import com.mysema.query.types.expr.NumberExpression;
 
 import javax.annotation.Generated;
 import javax.inject.Inject;
-
-import com.google.common.base.Function;
-import com.mysema.codegen.CodeWriter;
-import com.mysema.codegen.model.ClassType;
-import com.mysema.codegen.model.Constructor;
-import com.mysema.codegen.model.Parameter;
-import com.mysema.codegen.model.Type;
-import com.mysema.codegen.model.TypeCategory;
-import com.mysema.codegen.model.Types;
-import com.mysema.query.types.ConstructorExpression;
-import com.mysema.query.types.expr.NumberExpression;
+import java.io.IOException;
+import java.util.Set;
 
 /**
  * ProjectionSerializer is a {@link Serializer} implementation for projection types
@@ -60,7 +57,7 @@ public final class ProjectionSerializer implements Serializer{
 
         // imports
         writer.imports(NumberExpression.class.getPackage());
-        writer.imports(ConstructorExpression.class, Generated.class);
+        writer.imports(Expression.class, ConstructorExpression.class, Generated.class);
 
         // javadoc
         writer.javadoc(queryType + " is a Querydsl Projection type for " + simpleName);
@@ -85,14 +82,24 @@ public final class ProjectionSerializer implements Serializer{
         intro(model, writer);
 
         String localName = writer.getRawName(model);
+        Set<Integer> sizes = Sets.newHashSet();
 
         for (Constructor c : model.getConstructors()) {
+            final boolean asExpr = sizes.add(c.getParameters().size());
             // begin
             writer.beginConstructor(c.getParameters(), new Function<Parameter,Parameter>() {
                 @Override
                 public Parameter apply(Parameter p) {
-                    return new Parameter(p.getName(), typeMappings.getExprType(p.getType(),
-                            model, false, false, true));
+                    Type type;
+                    if (!asExpr) {
+                        type = typeMappings.getExprType(p.getType(),
+                                model, false, false, true);
+                    } else if (p.getType().isFinal()) {
+                        type = new ClassType(Expression.class, p.getType());
+                    } else {
+                        type = new ClassType(Expression.class, new TypeExtends(p.getType()));
+                    }
+                    return new Parameter(p.getName(), type);
                 }
             });
 
