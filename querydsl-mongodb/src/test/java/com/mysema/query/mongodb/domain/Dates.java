92261fdbc139c305dab131e427485c5050daca19
Timo Westkämper <timo.westkamper@mysema.com>
2014-05-04 10:54:33 +0300

    Add equals and hashCode

--- querydsl-mongodb/src/test/java/com/mysema/query/mongodb/domain/Dates.java
+++ querydsl-mongodb/src/test/java/com/mysema/query/mongodb/domain/Dates.java
@@ -1,10 +1,10 @@
 package com.mysema.query.mongodb.domain;
 
+import java.util.Date;
+
 import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
-
-import java.util.Date;
 
 @Entity
 public class Dates {
@@ -27,5 +27,30 @@ public class Dates {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Dates other = (Dates) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
