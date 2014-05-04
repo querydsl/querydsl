b1fe4484d2663eec5ea2e8243026565311b6a76d
Timo Westkämper <timo.westkamper@mysema.com>
2014-05-04 11:38:20 +0300

    Move id, equals and hashCode to common superclass

--- querydsl-mongodb/src/test/java/com/mysema/query/mongodb/domain/Dates.java
+++ querydsl-mongodb/src/test/java/com/mysema/query/mongodb/domain/Dates.java
@@ -2,24 +2,12 @@ package com.mysema.query.mongodb.domain;
 
 import java.util.Date;
 
-import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.Entity;
-import org.mongodb.morphia.annotations.Id;
 
 @Entity
-public class Dates {
-
-    private @Id ObjectId id;
+public class Dates extends AbstractEntity {
 
     private Date date;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
 
     public Date getDate() {
         return date;
@@ -29,28 +17,4 @@ public class Dates {
         this.date = date;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Dates other = (Dates) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
 }
--- querydsl-mongodb/src/test/java/com/mysema/query/mongodb/domain/User.java
+++ querydsl-mongodb/src/test/java/com/mysema/query/mongodb/domain/User.java
@@ -17,18 +17,14 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.Embedded;
 import org.mongodb.morphia.annotations.Entity;
-import org.mongodb.morphia.annotations.Id;
 import org.mongodb.morphia.annotations.Reference;
 
 @Entity
-public class User {
+public class User extends AbstractEntity {
 
     public enum Gender { MALE, FEMALE }
-
-    private @Id ObjectId id;
 
     private String firstName;
 
@@ -74,16 +70,8 @@ public class User {
 
     @Override
     public String toString() {
-        return "TestUser [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName
+        return "TestUser [id=" + getId() + ", firstName=" + firstName + ", lastName=" + lastName
                 + "]";
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
     }
 
     public String getFirstName() {
@@ -176,32 +164,5 @@ public class User {
     public void setEnemy(User enemy) {
         this.enemy = enemy;
     }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        User other = (User) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
-
 
 }
