18170440a0d8860bb9b03bdbe86086eb06d4f6b4
Timo Westkämper <timo.westkamper@gmail.com>
2014-06-19 22:00:46 +0300

    Fix script

--- travis/firebird.sh
+++ travis/firebird.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get install firebird2.5-superclassic
 sudo service firebird2.5-superclassic restart
-PASSWORD=`sudo cat /etc/firebird/2.5/SYSDBA.password | grep PASS | sed -e 's/.*PASSWORD="\([^"]*\)".*/\1/'`
-gsec -user SYSDBA -password $PASSWORD -modify sysdba -pw masterkey
-sudo isql-fb -r sysdba -pas masterkey -input firebird.sql
+#PASSWORD=`sudo cat /etc/firebird/2.5/SYSDBA.password | grep PASS | sed -e 's/.*PASSWORD="\([^"]*\)".*/\1/'`
+#gsec -user SYSDBA -password $PASSWORD -modify sysdba -pw masterkey
+sudo isql-fb -r sysdba -p masterkey -i travis/firebird.sql
