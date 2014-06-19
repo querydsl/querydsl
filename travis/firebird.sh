d17ef5edcb39dc21013657fdf8723fa5ffb5cada
Timo Westkämper <timo.westkamper@gmail.com>
2014-06-19 21:33:38 +0300

    Remove examples

--- travis/firebird.sh
+++ travis/firebird.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install firebird2.5-superclassic firebird2.5-examples
+sudo apt-get install firebird2.5-superclassic
 sudo service firebird2.5-superclassic restart
 PASSWORD=`sudo cat /etc/firebird/2.5/SYSDBA.password | grep PASS | sed -e 's/.*PASSWORD="\([^"]*\)".*/\1/'`
 gsec -user SYSDBA -password $PASSWORD -modify sysdba -pw masterkey
