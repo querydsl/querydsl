2492194761062d8021a4ca262bcd1dfbc3531697
Timo Westkämper <timo.westkamper@gmail.com>
2014-06-21 10:29:22 +0300

    Skip SYSDBA.password usage

--- travis/firebird.sh
+++ travis/firebird.sh
@@ -3,9 +3,10 @@ sudo apt-get install -qq firebird2.5-super firebird2.5-dev
 sudo sed /ENABLE_FIREBIRD_SERVER=/s/no/yes/ -i /etc/default/firebird2.5
 cat /etc/default/firebird2.5 | grep ENABLE_FIREBIRD_SERVER
 sudo service firebird2.5-super start
-#sudo service firebird2.5-superclassic restart
-PASSWORD=`sudo cat /etc/firebird/2.5/SYSDBA.password | grep PASS | sed -e 's/.*PASSWORD="\([^"]*\)".*/\1/'`
-sudo gsec -user sysdba -pass $PASSWORD -add querydsl -pw querydsl
+#PASSWORD=`sudo cat /etc/firebird/2.5/SYSDBA.password | grep PASS | sed -e 's/.*PASSWORD="\([^"]*\)".*/\1/'`
+echo Create user
+#sudo gsec -user sysdba -pass $PASSWORD -add querydsl -pw querydsl
+sudo gsec -user sysdba -pass masterkey -add querydsl -pw querydsl
 echo Create database
 sudo isql-fb -u querydsl -pas querydsl -i travis/firebird.sql -q
 #sudo isql-fb -u sysdba -pas $PASSWORD -i travis/firebird.sql -q
