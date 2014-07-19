718c1430895959024c5e0e47281d1f468741b97a
Timo Westkämper <timo.westkamper@gmail.com>
2014-07-19 20:12:43 +0300

    Add test for Hibernate model based codegen

---
+++ querydsl-jpa-codegen/src/test/java/com/mysema/query/jpa/domain17/CapiBCKeyedByGrundstueck.java
@@ -0,0 +1,5 @@
+package com.mysema.query.jpa.domain17;
+
+public abstract class CapiBCKeyedByGrundstueck extends CapiBusinessClass {
+
+}
---
+++ querydsl-jpa-codegen/src/test/java/com/mysema/query/jpa/domain17/DomainExporterTest.java
@@ -0,0 +1,26 @@
+package com.mysema.query.jpa.domain17;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.mysema.query.jpa.codegen.HibernateDomainExporter;
+import com.mysema.util.FileUtils;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+public class DomainExporterTest {
+
+    @Test
+    public void Execute() throws IOException {
+        File gen = new File("target/" + getClass().getSimpleName());
+        FileUtils.delete(gen);
+        Configuration config = new Configuration();
+        config.addFile(new File("src/test/resources/com/mysema/query/jpa/domain17/domain.hbm.xml"));
+        HibernateDomainExporter exporter = new HibernateDomainExporter("Q", gen, config);
+        exporter.execute();
+
+
+
+    }
+
+}
