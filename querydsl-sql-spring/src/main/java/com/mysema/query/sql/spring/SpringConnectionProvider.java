2b36037f84b4635f48265e78b0c7b58d62b592d0
Timo Westkämper <timo.westkamper@gmail.com>
2014-09-03 21:48:30 +0300

    Improve docs for sql-spring

--- querydsl-sql-spring/src/main/java/com/mysema/query/sql/spring/SpringConnectionProvider.java
+++ querydsl-sql-spring/src/main/java/com/mysema/query/sql/spring/SpringConnectionProvider.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2014, Timo Westkämper
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mysema.query.sql.spring;
 
 import javax.inject.Provider;
@@ -6,6 +19,17 @@ import java.sql.Connection;
 
 import org.springframework.jdbc.datasource.DataSourceUtils;
 
+/**
+ * SpringConnectionProvider is a Provider implementation which provides a transactionally bound connection
+ *
+ * <p>Usage example</p>
+ * <pre>
+ * {@code
+ * Provider<Connection> provider = new SpringConnectionProvider(dataSource());
+ * SQLQueryFactory queryFactory = SQLQueryFactory(configuration, provider);
+ * }
+ * </pre>
+ */
 public class SpringConnectionProvider implements Provider<Connection> {
     
     private final DataSource dataSource;
