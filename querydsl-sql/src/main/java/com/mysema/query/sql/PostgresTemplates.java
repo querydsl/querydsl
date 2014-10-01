8627b356bbc3ff1e05012c04f94ca7f849b797a8
Ruben Dijkstra <ruben@irp.nl>
2014-10-01 22:44:19 +0200

    Use the PostgreSQL 9.3 reserved word list in PostgresTemplates
    The list, taken from
    http://www.postgresql.org/docs/9.3/static/sql-keywords-appendix.html
    list a total of 100 reserved words

--- querydsl-sql/src/main/java/com/mysema/query/sql/PostgresTemplates.java
+++ querydsl-sql/src/main/java/com/mysema/query/sql/PostgresTemplates.java
@@ -29,13 +29,26 @@ import com.mysema.query.types.Ops;
  */
 public class PostgresTemplates extends SQLTemplates {
 
-    //TODO: Add all postgres specific reserved words
     private static final Set<String> POSTGRES_RESERVED_WORDS
-            = ImmutableSet.<String>builder()
-            .addAll(SQL_RESERVED_WORDS)
-            .add("ANALYSE",
-                    "ANALYZE")
-            .build();
+            = ImmutableSet.of(
+                    "ALL", "ANALYSE", "ANALYZE", "AND", "ANY", "ARRAY", "AS",
+                    "ASC", "ASYMMETRIC", "AUTHORIZATION", "BINARY", "BOTH",
+                    "CASE", "CAST", "CHECK", "COLLATE", "COLLATION", "COLUMN",
+                    "CONCURRENTLY", "CONSTRAINT", "CREATE", "CROSS",
+                    "CURRENT_CATALOG", "CURRENT_DATE", "CURRENT_ROLE",
+                    "CURRENT_SCHEMA", "CURRENT_TIME", "CURRENT_TIMESTAMP",
+                    "CURRENT_USER", "DEFAULT", "DEFERRABLE", "DESC", "DISTINCT",
+                    "DO", "ELSE", "END", "EXCEPT", "FALSE", "FETCH", "FOR",
+                    "FOREIGN", "FREEZE", "FROM", "FULL", "GRANT", "GROUP",
+                    "HAVING", "ILIKE", "IN", "INITIALLY", "INNER", "INTERSECT",
+                    "INTO", "IS", "ISNULL", "JOIN", "LATERAL", "LEADING", "LEFT",
+                    "LIKE", "LIMIT", "LOCALTIME", "LOCALTIMESTAMP", "NATURAL",
+                    "NOT", "NOTNULL", "NULL", "OFFSET", "ON", "ONLY", "OR",
+                    "ORDER", "OUTER", "OVER", "OVERLAPS", "PLACING", "PRIMARY",
+                    "REFERENCES", "RETURNING", "RIGHT", "SELECT", "SESSION_USER",
+                    "SIMILAR", "SOME", "SYMMETRIC", "TABLE", "THEN", "TO",
+                    "TRAILING", "TRUE", "UNION", "UNIQUE", "USER", "USING",
+                    "VARIADIC", "VERBOSE", "WHEN", "WHERE", "WINDOW", "WITH");
 
     public static Builder builder() {
         return new Builder() {
