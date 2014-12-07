fb2cea9c17c3976da0f3c5467ffbc0c517280dd7
Timo Westkämper <timo.westkamper@gmail.com>
2014-12-07 22:54:13 +0200

    Replace proprietary annotation

--- querydsl-jpa/src/test/java/com/mysema/query/jpa/domain/Cat.java
+++ querydsl-jpa/src/test/java/com/mysema/query/jpa/domain/Cat.java
@@ -13,18 +13,10 @@
  */
 package com.mysema.query.jpa.domain;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-
-import org.hibernate.annotations.IndexColumn;
 
 /**
  * The Class Cat.
@@ -39,7 +31,7 @@ public class Cat extends Animal {
 
     @OneToMany
     @JoinTable(name = "kittens", joinColumns = @JoinColumn(name = "cat_id"), inverseJoinColumns = @JoinColumn(name = "kitten_id"))
-    @IndexColumn(name = "ind")
+    @OrderColumn(name = "ind")
     private List<Cat> kittens = new ArrayList<Cat>();
 
     @OneToMany
