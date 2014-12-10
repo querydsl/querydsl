53daa02e8bb97dfec8c21d66a26516116c9b60e8
Timo Westkämper <timo.westkamper@gmail.com>
2014-12-10 19:51:02 +0200

    Fix behaviour on Boolean value

--- querydsl-sql/src/test/java/com/mysema/query/SelectMySQLBase.java
+++ querydsl-sql/src/test/java/com/mysema/query/SelectMySQLBase.java
@@ -1,12 +1,12 @@
 package com.mysema.query;
 
-import static com.mysema.query.Constants.survey;
-import static com.mysema.query.Target.MYSQL;
-
-import org.junit.Test;
-
+import com.mysema.query.sql.domain.QNumberTest;
 import com.mysema.query.sql.mysql.MySQLQuery;
 import com.mysema.testutil.IncludeIn;
+import org.junit.Test;
+import static com.mysema.query.Constants.survey;
+import static com.mysema.query.Target.MYSQL;
+import static org.junit.Assert.assertEquals;
 
 
 public class SelectMySQLBase extends AbstractBaseTest {
@@ -28,6 +28,17 @@ public class SelectMySQLBase extends AbstractBaseTest {
         mysqlQuery().from(survey).lockInShareMode().list(survey.id);
         mysqlQuery().from(survey).smallResult().list(survey.id);
         mysqlQuery().from(survey).straightJoin().list(survey.id);
+    }
+
+    @Test
+    @IncludeIn(MYSQL)
+    public void Tinyint() {
+        QNumberTest numberTest = QNumberTest.numberTest;
+        delete(numberTest).execute();
+        insert(numberTest).set(numberTest.col1Boolean, true).execute();
+        insert(numberTest).set(numberTest.col1Number, 1).execute();
+        assertEquals(2, query().from(numberTest).list(numberTest.col1Boolean).size());
+        assertEquals(2, query().from(numberTest).list(numberTest.col1Number).size());
     }
 
 }
