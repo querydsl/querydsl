cb16f386dbddf69189a105a5471f3ba4abd518b8
Timo Westkämper <timo.westkamper@gmail.com>
2014-12-14 12:34:35 +0200

    Fix SQL Server tests

--- querydsl-sql/src/test/java/com/mysema/query/SelectBase.java
+++ querydsl-sql/src/test/java/com/mysema/query/SelectBase.java
@@ -13,12 +13,23 @@
  */
 package com.mysema.query;
 
+import static com.mysema.query.Constants.*;
+import static com.mysema.query.Target.*;
+import static org.junit.Assert.*;
+
 import java.io.*;
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.LocalTime;
+import org.junit.Ignore;
+import org.junit.Test;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
@@ -39,16 +50,8 @@ import com.mysema.query.types.query.NumberSubQuery;
 import com.mysema.query.types.template.NumberTemplate;
 import com.mysema.testutil.ExcludeIn;
 import com.mysema.testutil.IncludeIn;
+
 import junit.framework.Assert;
-import org.joda.time.DateTime;
-import org.joda.time.LocalDate;
-import org.joda.time.LocalDateTime;
-import org.joda.time.LocalTime;
-import org.junit.Ignore;
-import org.junit.Test;
-import static com.mysema.query.Constants.*;
-import static com.mysema.query.Target.*;
-import static org.junit.Assert.*;
 
 public class SelectBase extends AbstractBaseTest {
 
@@ -625,7 +628,7 @@ public class SelectBase extends AbstractBaseTest {
     }
 
     @Test
-    @ExcludeIn({FIREBIRD})
+    @ExcludeIn({FIREBIRD, SQLSERVER})
     public void GroupBy_Distinct_Count() {
         List<Integer> ids = query().from(employee).groupBy(employee.id).distinct().list(NumberTemplate.ONE);
         SearchResults<Integer> results = query().from(employee).groupBy(employee.id)
