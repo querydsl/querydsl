b306f74075cf4495c85aa3df9ec5e7aa6bdf0187
Timo Westkämper <timo.westkamper@gmail.com>
2015-02-28 19:26:20 +0200

    Improve DB2 patterns

--- querydsl-sql/src/main/java/com/querydsl/sql/DB2Templates.java
+++ querydsl-sql/src/main/java/com/querydsl/sql/DB2Templates.java
@@ -144,6 +144,15 @@ public class DB2Templates extends SQLTemplates {
         setNullsFirst(null);
         setNullsLast(null);
 
+        setPrecedence(7, Ops.CONCAT);
+        setPrecedence(19, Ops.IS_NULL, Ops.IS_NOT_NULL, Ops.LIKE, Ops.LIKE_ESCAPE, Ops.BETWEEN,
+                Ops.IN, Ops.NOT_IN, Ops.EXISTS);
+
+        // other like cases
+        setPrecedence(19, Ops.ENDS_WITH, Ops.ENDS_WITH_IC,
+                Ops.STARTS_WITH, Ops.STARTS_WITH_IC,
+                Ops.STRING_CONTAINS, Ops.STRING_CONTAINS_IC);
+
         add(SQLOps.NEXTVAL, "next value for {0s}");
 
         add(Ops.MathOps.RANDOM, "rand()");
--- querydsl-sql/src/test/java/com/querydsl/sql/DB2TemplatesTest.java
+++ querydsl-sql/src/test/java/com/querydsl/sql/DB2TemplatesTest.java
@@ -13,11 +13,42 @@
  */
 package com.querydsl.sql;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.querydsl.core.types.Ops;
+
 public class DB2TemplatesTest extends AbstractSQLTemplatesTest{
 
     @Override
     protected SQLTemplates createTemplates() {
         return new DB2Templates();
+    }
+
+    @Test
+    public void Precedence() {
+        // Expressions within parentheses are evaluated first. When the order of evaluation is not
+        // specified by parentheses, prefix operators are applied before multiplication and division,
+        // and multiplication, division, and concatenation are applied before addition and subtraction.
+        // Operators at the same precedence level are applied from left to right.
+
+        int p1 = getPrecedence(Ops.NEGATE);
+        int p2 = getPrecedence(Ops.MULT, Ops.DIV, Ops.CONCAT);
+        int p3 = getPrecedence(Ops.ADD, Ops.SUB);
+        int p4 = getPrecedence(Ops.EQ, Ops.NE, Ops.LT, Ops.GT, Ops.LOE, Ops.GOE);
+        int p5 = getPrecedence(Ops.IS_NULL, Ops.IS_NOT_NULL, Ops.LIKE, Ops.LIKE_ESCAPE, Ops.BETWEEN, Ops.IN, Ops.NOT_IN, Ops.EXISTS);
+        int p6 = getPrecedence(Ops.NOT);
+        int p7 = getPrecedence(Ops.AND);
+        int p8 = getPrecedence(Ops.OR);
+
+        assertTrue(p1 < p2);
+        assertTrue(p2 < p3);
+        assertTrue(p3 < p4);
+        assertTrue(p4 < p5);
+        assertTrue(p5 < p6);
+        assertTrue(p6 < p7);
+        assertTrue(p7 < p8);
     }
 
 }
