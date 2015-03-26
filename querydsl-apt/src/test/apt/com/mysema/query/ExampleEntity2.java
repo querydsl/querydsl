a52d9a774dc74c9d4c99d2820300cfde97126457
Ruben Dijkstra <rubendijkstra123@gmail.com>
2015-03-26 21:24:35 +0100

    Add fix for extending external types

---
+++ querydsl-apt/src/test/apt/com/mysema/query/ExampleEntity2.java
@@ -0,0 +1,9 @@
+package com.mysema.query;
+
+import com.mysema.query.annotations.QueryEntity;
+import com.mysema.query.domain.AbstractEntity;
+
+@QueryEntity
+public class ExampleEntity2 extends AbstractEntity<ExampleEntity2> {
+// id inherited
+}
