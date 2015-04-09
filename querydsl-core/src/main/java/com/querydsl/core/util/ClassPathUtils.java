b7ed17c0fc90ec3a007f7bcbc4ae90130287ed55
Baris Cubukcuoglu <cubukcuoglu.baris@gmail.com>
2015-04-09 21:10:54 +0200

    Changed "scanPackage" method implementation. Used Reflections API.

--- querydsl-core/src/main/java/com/querydsl/core/util/ClassPathUtils.java
+++ querydsl-core/src/main/java/com/querydsl/core/util/ClassPathUtils.java
@@ -13,24 +13,21 @@
  */
 package com.querydsl.core.util;
 
-import java.io.File;
+import org.reflections.Reflections;
+import org.reflections.adapters.JavaReflectionAdapter;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+
 import java.io.IOException;
-import java.net.JarURLConnection;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 /**
  * ClassPathUtils provides classpath scanning functionality
  *
  * @author tiwe
- *
  */
 public final class ClassPathUtils {
 
@@ -39,75 +36,19 @@ public final class ClassPathUtils {
     }
 
     public static Set<Class<?>> scanPackage(ClassLoader classLoader, String pkg) throws IOException {
-        String packagePath = pkg.replace('.', '/');
-        Enumeration<URL> urls = classLoader.getResources(packagePath);
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .addUrls(ClasspathHelper.forPackage(pkg, classLoader))
+                .addClassLoader(classLoader)
+                .setScanners(new SubTypesScanner(false))
+                .setMetadataAdapter(new JavaReflectionAdapter()));
         Set<Class<?>> classes = new HashSet<Class<?>>();
-        while (urls.hasMoreElements()) {
-            URL url = urls.nextElement();
-            if (url.getProtocol().equals("jar")) {
-                scanJar(classLoader, classes, url, packagePath);
-
-            } else if (url.getProtocol().equals("file")) {
-                scanDirectory(classLoader, pkg, classes, url, pkg);
-
-            } else {
-                throw new IllegalArgumentException("Illegal url : " + url);
+        for (String typeNames : reflections.getStore().get(SubTypesScanner.class.getSimpleName()).values()) {
+            Class<?> clazz = safeClassForName(classLoader, typeNames);
+            if (clazz != null) {
+                classes.add(clazz);
             }
         }
         return classes;
-    }
-
-    private static void scanDirectory(ClassLoader classLoader, String pkg, Set<Class<?>> classes,
-            URL url, String packageName) throws IOException {
-        Deque<File> files = new ArrayDeque<File>();
-        String packagePath;
-        try {
-            File packageAsFile = new File(url.toURI());
-            packagePath = packageAsFile.getPath();
-            files.add(packageAsFile);
-        } catch (URISyntaxException e) {
-            throw new IOException(e);
-        }
-        while (!files.isEmpty()) {
-            File file = files.pop();
-            for (File child : file.listFiles()) {
-                if (child.getName().endsWith(".class")) {
-                    String fileName = child.getPath().substring(packagePath.length()+1).replace(File.separatorChar, '.');
-                    String className = pkg + "." + fileName.substring(0, fileName.length()-6);
-                    if (className.startsWith(packageName)) {
-                        Class<?> cl = safeClassForName(classLoader, className);
-                        if (cl != null) {
-                            classes.add(cl);
-                        }
-                    }
-                } else if (child.isDirectory()) {
-                    files.add(child);
-                }
-            }
-        }
-    }
-
-    private static void scanJar(ClassLoader classLoader, Set<Class<?>> classes, URL url,
-            String packagePath) throws IOException {
-        // See http://stackoverflow.com/a/402771/14731
-        JarURLConnection connection = (JarURLConnection) url.openConnection();
-        JarFile jarFile = connection.getJarFile();
-        try {
-            Enumeration<JarEntry> entries = jarFile.entries();
-            while (entries.hasMoreElements()) {
-                JarEntry entry = entries.nextElement();
-                if (entry.getName().endsWith(".class") && entry.getName().startsWith(packagePath)
-                        && entry.getName().startsWith(connection.getEntryName())) {
-                    String className = entry.getName().substring(0, entry.getName().length()-6).replace('/', '.');
-                    Class<?> cl = safeClassForName(classLoader, className);
-                    if (cl != null) {
-                        classes.add(cl);
-                    }
-                }
-            }
-        } finally {
-            jarFile.close();
-        }
     }
 
     public static Class<?> safeClassForName(ClassLoader classLoader, String className) {
@@ -125,5 +66,4 @@ public final class ClassPathUtils {
     }
 
     private ClassPathUtils() {}
-
 }
