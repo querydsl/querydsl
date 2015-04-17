48069695e794226a28eda82b03383186aba0930b
Timo Westkämper <timo.westkamper@gmail.com>
2015-04-17 21:24:06 +0300

    Fix test

--- querydsl-sql/src/test/java/com/querydsl/sql/SelectBase.java
+++ querydsl-sql/src/test/java/com/querydsl/sql/SelectBase.java
@@ -233,7 +233,7 @@ public class SelectBase extends AbstractBaseTest {
 
     @Test
     public void Case() {
-        NumberExpression<Float> numExpression = employee.salary.floatValue().divide(employee2.salary.floatValue()).multiply(100);
+        NumberExpression<Float> numExpression = employee.salary.floatValue().divide(employee2.salary.floatValue()).multiply(100.1);
         NumberExpression<Float> numExpression2 = employee.id.when(0).then(0.0F).otherwise(numExpression);
         assertEquals(ImmutableList.of(87, 90, 88, 87, 83, 80, 75),
                 query().from(employee, employee2)
