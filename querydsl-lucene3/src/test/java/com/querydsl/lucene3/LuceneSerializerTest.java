90fbc3d8c6b6a6e8b40d7debdc61414b6a202581
John Tims <john.k.tims@gmail.com>
2015-05-09 00:56:33 -0400

    Remove trailing whitespace in querydsl-lucene3

--- querydsl-lucene3/src/test/java/com/querydsl/lucene3/LuceneSerializerTest.java
+++ querydsl-lucene3/src/test/java/com/querydsl/lucene3/LuceneSerializerTest.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2015, The Querydsl Team (http://www.querydsl.com/team)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -118,7 +118,7 @@ public class LuceneSerializerTest {
         floatField = entityPath.getNumber("floatField", Float.class);
 
         idx = new RAMDirectory();
-        config = new IndexWriterConfig(Version.LUCENE_31, 
+        config = new IndexWriterConfig(Version.LUCENE_31,
                 new StandardAnalyzer(Version.LUCENE_30))
             .setOpenMode(IndexWriterConfig.OpenMode.CREATE);
         writer = new IndexWriter(idx, config);
