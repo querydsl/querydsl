1ad39860c763148914c29f525c8fabf7131fa4a9
Timo Westkämper <timo.westkamper@gmail.com>
2015-05-29 22:20:44 +0300

    Change equals contract

--- src/test/java/com/mysema/codegen/model/ParameterTest.java
+++ src/test/java/com/mysema/codegen/model/ParameterTest.java
@@ -6,13 +6,8 @@
 package com.mysema.codegen.model;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
-import com.mysema.codegen.model.ClassType;
-import com.mysema.codegen.model.Parameter;
-import com.mysema.codegen.model.TypeCategory;
 
 public class ParameterTest {
 
@@ -23,8 +18,9 @@ public class ParameterTest {
         Parameter param3 = new Parameter("test2",
                 new ClassType(TypeCategory.NUMERIC, Integer.class));
 
-        assertTrue(param1.equals(param2));
+        assertFalse(param1.equals(param2));
         assertFalse(param1.equals(param3));
         assertFalse(param2.equals(param3));
     }
+
 }
