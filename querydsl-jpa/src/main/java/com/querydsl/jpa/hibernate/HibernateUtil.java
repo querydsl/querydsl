cfe4c0c4ba52c0035097d85e68e52a10e7e0ff42
Timo Westkämper <timo.westkamper@gmail.com>
2015-07-17 21:03:39 +0300

    Fix parameter handling

--- querydsl-jpa/src/main/java/com/querydsl/jpa/hibernate/HibernateUtil.java
+++ querydsl-jpa/src/main/java/com/querydsl/jpa/hibernate/HibernateUtil.java
@@ -16,12 +16,14 @@ package com.querydsl.jpa.hibernate;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.hibernate.Query;
 import org.hibernate.type.*;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.querydsl.core.types.ParamExpression;
 import com.querydsl.core.types.ParamNotSetException;
 import com.querydsl.core.types.dsl.Param;
@@ -34,17 +36,26 @@ import com.querydsl.core.types.dsl.Param;
  */
 public final class HibernateUtil {
 
-    private static final Map<Class<?>,Type> TYPES = new HashMap<Class<?>,Type>();
+    private static final Set<Class<?>> BUILT_IN = ImmutableSet.of(Boolean.class, Byte.class,
+            Character.class, Double.class, Float.class, Integer.class, Long.class, Short.class,
+            String.class, BigDecimal.class, byte[].class, Byte[].class, java.util.Date.class,
+            java.util.Calendar.class, java.sql.Date.class, java.sql.Time.class, java.sql.Timestamp.class,
+            java.util.Locale.class, java.util.TimeZone.class, java.util.Currency.class, Class.class,
+            java.io.Serializable.class, java.sql.Blob.class, java.sql.Clob.class);
+
+    private static final Map<Class<?>, Type> NUMERIC_TYPES;
 
     static {
-        TYPES.put(Byte.class, new ByteType());
-        TYPES.put(Short.class, new ShortType());
-        TYPES.put(Integer.class, new IntegerType());
-        TYPES.put(Long.class, new LongType());
-        TYPES.put(BigInteger.class, new BigIntegerType());
-        TYPES.put(Double.class, new DoubleType());
-        TYPES.put(Float.class, new FloatType());
-        TYPES.put(BigDecimal.class, new BigDecimalType());
+        ImmutableMap.Builder<Class<?>, Type> builder = ImmutableMap.builder();
+        builder.put(Byte.class, new ByteType());
+        builder.put(Short.class, new ShortType());
+        builder.put(Integer.class, new IntegerType());
+        builder.put(Long.class, new LongType());
+        builder.put(BigInteger.class, new BigIntegerType());
+        builder.put(Double.class, new DoubleType());
+        builder.put(Float.class, new FloatType());
+        builder.put(BigDecimal.class, new BigDecimalType());
+        NUMERIC_TYPES = builder.build();
     }
 
     private HibernateUtil() { }
@@ -67,10 +78,10 @@ public final class HibernateUtil {
     private static void setValue(Query query, String key, Object val) {
         if (val instanceof Collection<?>) {
             query.setParameterList(key, (Collection<?>) val);
-        } else if (val.getClass().isArray()) {
+        } else if (val instanceof Object[] && !BUILT_IN.contains(val.getClass())) {
             query.setParameterList(key, (Object[]) val);
-        } else if (TYPES.containsKey(val.getClass())) {
-            query.setParameter(key, val, TYPES.get(val.getClass()));
+        } else if (NUMERIC_TYPES.containsKey(val.getClass())) {
+            query.setParameter(key, val, NUMERIC_TYPES.get(val.getClass()));
         } else {
             query.setParameter(key, val);
         }
