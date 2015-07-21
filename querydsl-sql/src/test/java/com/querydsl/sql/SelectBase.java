226684019bdd89b8549b8a53cbb22ef1eac178af
Timo Westkämper <timo.westkamper@gmail.com>
2015-07-21 21:11:41 +0300

    Fix tests

--- querydsl-sql/src/test/java/com/querydsl/sql/SelectBase.java
+++ querydsl-sql/src/test/java/com/querydsl/sql/SelectBase.java
@@ -367,7 +367,7 @@ public class SelectBase extends AbstractBaseTest {
     public void Count_All_Oracle() {
         expectedQuery = "select count(*) rc from EMPLOYEE e";
         NumberPath<Long> rowCount = Expressions.numberPath(Long.class, "rc");
-        assertEquals(0, query().from(employee).select(Wildcard.count.as(rowCount)).fetchOne().intValue());
+        assertEquals(10, query().from(employee).select(Wildcard.count.as(rowCount)).fetchOne().intValue());
     }
 
     @Test
