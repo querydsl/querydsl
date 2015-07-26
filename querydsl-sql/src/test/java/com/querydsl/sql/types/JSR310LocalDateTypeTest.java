dc648e9518650ec4ae084136af5b6bfd3c411e44
Timo Westkämper <timo.westkamper@gmail.com>
2015-07-26 15:52:59 +0300

    Fix test

--- querydsl-sql/src/test/java/com/querydsl/sql/types/JSR310LocalDateTypeTest.java
+++ querydsl-sql/src/test/java/com/querydsl/sql/types/JSR310LocalDateTypeTest.java
@@ -24,7 +24,7 @@ public class JSR310LocalDateTypeTest extends AbstractJSR310DateTimeTypeTest<Loca
         Date date = Date.valueOf(value);
 
         PreparedStatement stmt = EasyMock.createNiceMock(PreparedStatement.class);
-        stmt.setDate(1, date, UTC);
+        stmt.setDate(1, date);
         EasyMock.replay(stmt);
 
         type.setValue(stmt, 1, value);
@@ -34,7 +34,7 @@ public class JSR310LocalDateTypeTest extends AbstractJSR310DateTimeTypeTest<Loca
     @Test
     public void Get() throws SQLException {
         ResultSet resultSet = EasyMock.createNiceMock(ResultSet.class);
-        EasyMock.expect(resultSet.getDate(1, UTC)).andReturn(new Date(UTC.getTimeInMillis()));
+        EasyMock.expect(resultSet.getDate(1)).andReturn(new Date(UTC.getTimeInMillis()));
         EasyMock.replay(resultSet);
 
         LocalDate result = type.getValue(resultSet, 1);
