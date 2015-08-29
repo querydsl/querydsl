813d32a250d1eaefb1bb728bc18988a6316002b5
Timo Westkämper <timo.westkamper@gmail.com>
2015-08-29 21:03:28 +0300

    Expand args

--- querydsl-core/src/main/java/com/querydsl/core/types/FactoryExpressionUtils.java
+++ querydsl-core/src/main/java/com/querydsl/core/types/FactoryExpressionUtils.java
@@ -49,7 +49,7 @@ public final class FactoryExpressionUtils {
         FactoryExpressionAdapter(FactoryExpression<T> inner, List<Expression<?>> args) {
             super(inner.getType());
             this.inner = inner;
-            this.args = args;
+            this.args = expand(args);
         }
 
         @Override
