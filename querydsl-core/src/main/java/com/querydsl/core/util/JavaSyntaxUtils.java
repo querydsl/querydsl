5ad7a504f780c27ac47d72fb549d0c0c2d914e1e
Ruben Dijkstra <rubendijkstra123@gmail.com>
2016-01-25 17:44:46 +0100

    Use the keyword list from SourceVersion
    Using SourceVersion.isKeyword(), we don't have to update the list of reserved words.

--- querydsl-core/src/main/java/com/querydsl/core/util/JavaSyntaxUtils.java
+++ querydsl-core/src/main/java/com/querydsl/core/util/JavaSyntaxUtils.java
@@ -13,9 +13,7 @@
  */
 package com.querydsl.core.util;
 
-import java.util.Set;
-
-import com.google.common.collect.ImmutableSet;
+import javax.lang.model.SourceVersion;
 
 /**
  * JavaSyntaxUtils defines reserved keywords for Java
@@ -27,63 +25,8 @@ public final class JavaSyntaxUtils {
 
     private JavaSyntaxUtils() { }
 
-    private static final Set<String> reserved = ImmutableSet.of(
-    "abstract",
-    "assert",
-    "boolean",
-    "break",
-    "byte",
-    "case",
-    "catch",
-    "char",
-    "class",
-    "const",
-    "continue",
-    "default",
-    "double",
-    "do",
-    "else",
-    "enum",
-    "extends",
-    "false",
-    "final",
-    "finally",
-    "float",
-    "for",
-    "goto",
-    "if",
-    "implements",
-    "import",
-    "instanceof",
-    "int",
-    "interface",
-    "long",
-    "native",
-    "new",
-    "null",
-    "package",
-    "private",
-    "protected",
-    "public",
-    "return",
-    "short",
-    "static",
-    "strictfp",
-    "super",
-    "switch",
-    "synchronized",
-    "this",
-    "throw",
-    "throws",
-    "transient",
-    "true",
-    "try",
-    "void",
-    "volatile",
-    "while");
-
     public static boolean isReserved(String str) {
-        return reserved.contains(str);
+        return SourceVersion.isKeyword(str);
     }
 
 }
