2814e8dc79a6e5fa3df75393ba8f73f80045cd2c
Ruben Dijkstra <rubendijkstra123@gmail.com>
2016-03-01 19:59:24 +0100

    Adjust the prompt at release to remind releasing querydsl-contrib
    [skip ci]

--- release.sh
+++ release.sh
@@ -4,3 +4,5 @@ git pull
 mvn versions:set -DgenerateBackupPoms=false
 mvn clean deploy -DskipTests -Dgpg.skip=false
 ./dist.sh
+echo -e "Don't forget \x1b[33mquerydsl-contrib\x1b[m."
+echo -e "After deploying the \x1b[33mreference documentation\x1b[m, update the \x1b[33mcurrent symlink\x1b[m"
