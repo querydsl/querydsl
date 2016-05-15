fe7df18037510ecd5c86a0d8cf77b191fc9d4a59
Timo Westkämper <timo.westkamper@gmail.com>
2016-05-15 23:48:58 +0300

    Querydsl 3.7.3

--- querydsl-codegen/src/main/java/com/mysema/query/codegen/DefaultVariableNameFunction.java
+++ querydsl-codegen/src/main/java/com/mysema/query/codegen/DefaultVariableNameFunction.java
@@ -15,6 +15,7 @@
  */
 package com.mysema.query.codegen;
 
+import com.google.common.base.Function;
 import com.mysema.codegen.StringUtils;
 import com.mysema.util.JavaSyntaxUtils;
 
@@ -22,7 +23,7 @@ import com.mysema.util.JavaSyntaxUtils;
  * Default variable name generation strategy which un-capitalizes the first letter of the class name.
  *
  */
-public final class DefaultVariableNameFunction implements NamingFunction {
+public final class DefaultVariableNameFunction implements Function<EntityType, String> {
 
     public static final DefaultVariableNameFunction INSTANCE = new DefaultVariableNameFunction();
 
