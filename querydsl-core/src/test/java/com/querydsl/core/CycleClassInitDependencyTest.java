0d2a6e4eba897e1a4f57aa987779aae183f3b7ce
Ruben Dijkstra <rubendijkstra123@gmail.com>
2016-07-19 15:03:02 +0200

    Extract class for Thread Safety checks

--- querydsl-core/src/test/java/com/querydsl/core/CycleClassInitDependencyTest.java
+++ querydsl-core/src/test/java/com/querydsl/core/CycleClassInitDependencyTest.java
@@ -9,6 +9,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.reflections.util.ClasspathHelper;
 
+import com.querydsl.core.testutil.ThreadSafety;
+
 public class CycleClassInitDependencyTest {
 
     private static ClassLoader loader;
@@ -27,16 +29,12 @@ public class CycleClassInitDependencyTest {
     }
 
     @Test(timeout = 2000)
-    public void test() throws InterruptedException {
+    public void test() {
 
         // each thread wants to load one part of the dependency circle
-        Thread t1 = new Thread(new LoadClassRunnable("com.querydsl.core.types.dsl.NumberExpression"));
-        Thread t2 = new Thread(new LoadClassRunnable("com.querydsl.core.types.dsl.Expressions"));
-        t1.start();
-        t2.start();
-
-        t1.join();
-
+        ThreadSafety.check(
+                new LoadClassRunnable("com.querydsl.core.types.dsl.NumberExpression"),
+                new LoadClassRunnable("com.querydsl.core.types.dsl.Expressions"));
     }
 
     private static class LoadClassRunnable implements Runnable {
