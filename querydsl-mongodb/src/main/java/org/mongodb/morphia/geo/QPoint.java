b246805c6a05fd48bfdb258c324110f400ef132e
Javad Moghisi <j.moghisi@gmail.com>
2016-09-02 00:00:21 +0100

    update QPoint to implement Path

--- querydsl-mongodb/src/main/java/org/mongodb/morphia/geo/QPoint.java
+++ querydsl-mongodb/src/main/java/org/mongodb/morphia/geo/QPoint.java
@@ -1,31 +1,55 @@
 package org.mongodb.morphia.geo;
 
 import javax.annotation.Nullable;
+import java.lang.reflect.AnnotatedElement;
 
-import com.querydsl.core.types.PathMetadata;
+import com.querydsl.core.types.*;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.EntityPathBase;
-import com.querydsl.core.types.dsl.PathInits;
+import com.querydsl.core.types.dsl.SimpleExpression;
 import com.querydsl.mongodb.morphia.MorphiaExpressions;
 
 /**
- * {@code QPoint} is an adapter type for Point to use geo spatial querying features of Mongodb
+ * {@code QPoint} represents Point paths, needed to support geo spatial querying features of Mongodb
  *
  * @author jmoghisi
  *
  */
-public class QPoint extends EntityPathBase<Point> {
+public class QPoint extends SimpleExpression<Point> implements Path<Point> {
 
-    public QPoint(String variable) {
-        super(Point.class, variable);
+    private final PathImpl<Point> pathMixin;
+
+    public QPoint(String var) {
+        this(PathMetadataFactory.forVariable(var));
+    }
+
+    public QPoint(Path<?> parent, String property) {
+        this(PathMetadataFactory.forProperty(parent, property));
     }
 
     public QPoint(PathMetadata metadata) {
-        super(Point.class, metadata);
+        super(ExpressionUtils.path(Point.class, metadata));
+        this.pathMixin = (PathImpl<Point>) mixin;
     }
 
-    public QPoint(PathMetadata metadata, @Nullable PathInits inits) {
-        super(Point.class, metadata, inits);
+    @Override
+    public PathMetadata getMetadata() {
+        return pathMixin.getMetadata();
+    }
+
+    @Override
+    public Path<?> getRoot() {
+        return pathMixin.getRoot();
+    }
+
+    @Override
+    public AnnotatedElement getAnnotatedElement() {
+        return pathMixin.getAnnotatedElement();
+    }
+
+    @Nullable
+    @Override
+    public <R, C> R accept(Visitor<R, C> v, @Nullable C context) {
+        return v.visit(pathMixin, context);
     }
 
     public BooleanExpression near(Point point) {
@@ -35,4 +59,5 @@ public class QPoint extends EntityPathBase<Point> {
     public BooleanExpression nearSphere(Point point) {
         return MorphiaExpressions.nearSphere(this, point);
     }
+
 }
