88c2c9d5f27e0c73810422f7b6253702a99b45a3
John Tims <john.k.tims@gmail.com>
2019-11-15 14:52:32 -0700

    Remove cubrid in tests

--- travis/docker_bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-docker-compose --file travis/docker-compose.yml up -d mysql postgresql firebird cubrid mongo
-docker exec $(docker-compose --file travis/docker-compose.yml ps -q cubrid) /bin/bash -c 'mkdir -p ~/CUBRID_databases/demodb && cd $_ && cubrid createdb --db-volume-size=100M --log-volume-size=100M demodb en_US.iso88591 && cubrid loaddb -u dba -s $CUBRID/demo/demodb_schema -d $CUBRID/demo/demodb_objects demodb'
-docker exec -d $(docker-compose --file travis/docker-compose.yml ps -q cubrid) /bin/bash -c 'cubrid server start demodb'
-docker ps
