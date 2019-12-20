ad6ee152e893039c3af9d3e6943c75d1bbd59a7d
Ido Salomon <ido.salomon@duda.co>
2019-12-20 22:47:45 +0200

    Optimize junit.Assert imports

--- querydsl-jpa/src/test/java/com/querydsl/jpa/JPABase.java
+++ querydsl-jpa/src/test/java/com/querydsl/jpa/JPABase.java
@@ -15,7 +15,10 @@ package com.querydsl.jpa;
 
 import static com.querydsl.jpa.JPAExpressions.selectFrom;
 import static com.querydsl.jpa.JPAExpressions.selectOne;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.sql.Connection;
 import java.util.HashMap;
