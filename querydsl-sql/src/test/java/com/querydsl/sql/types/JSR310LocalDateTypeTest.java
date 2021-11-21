b41213cae5653080126e1d3eaf8886fc5d5ff19a
mengxuan <mz2yu@virginia.edu>
2021-11-21 15:52:27 -0600

    Update MathExpressions class and add test

--- querydsl-sql/src/test/java/com/querydsl/sql/types/JSR310LocalDateTypeTest.java
+++ querydsl-sql/src/test/java/com/querydsl/sql/types/JSR310LocalDateTypeTest.java
@@ -3,14 +3,14 @@ package com.querydsl.sql.types;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.ZoneOffset;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.util.TimeZone;
 
 import org.easymock.EasyMock;
+import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
 
 public class JSR310LocalDateTypeTest extends AbstractJSR310DateTimeTypeTest<LocalDate> {
@@ -57,4 +57,41 @@ public class JSR310LocalDateTypeTest extends AbstractJSR310DateTimeTypeTest<Loca
         assertNotNull(result);
         assertTrue(result.toEpochDay() == 0);
     }
+    /**
+     * Unit Test for issue #3001
+     * JSR310LocalDateType produces incorrect value for 2014-03-31
+     * dummyTest1 is testing the getValue() method with a result set includes 2014/03/31
+     * the getDayOfMonth() method in dummyTest1 should return 31, but it returns 30 which shown as a bug
+     * dummyTest2 is testing the getValue() method with a result set doesn't include 2014/03/31
+     * the getDayOfMonth() method in dummyTest2 return the value correctly
+     * dummyTestNull is testing the getValue() method with Null, which returns Null
+     */
+
+    @Test
+    public void dummyTest1() throws SQLException, ParseException {
+        ResultSet resultSet = EasyMock.createNiceMock(ResultSet.class);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //format.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        java.util.Date date = format.parse("2014-03-31 00:00:00");
+        long millis = date.getTime();
+
+        EasyMock.expect(resultSet.getDate(1, UTC)).andReturn(new Date(millis));
+
+
+        EasyMock.replay(resultSet);
+
+        @Nullable LocalDate result = type.getValue(resultSet, 1);
+        System.out.println(result);
+
+        EasyMock.verify(resultSet);
+
+        assertNotNull(result);
+        //Test for value of month
+        assertTrue(result.getMonthValue() == 3);
+        //Test for day of month, excepted 31, got 30
+        assertTrue(result.getDayOfMonth() == 31);
+    }
+
+
 }
